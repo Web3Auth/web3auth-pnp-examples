e2952c7513c89d318243e3a1f48e20473351fa67
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2022-07-26 23:44:21 +0800

    Add evm chains for web3auth modal

---
+++ web-modal-sdk/evm/angular-evm-web3auth-example/src/app/app.component.ts
@@ -0,0 +1,126 @@
+import { Component } from "@angular/core";
+import { Web3Auth } from "@web3auth/web3auth";
+import { CHAIN_NAMESPACES, SafeEventEmitterProvider } from "@web3auth/base";
+import RPC from "./web3RPC"; // for using web3.js
+// import RPC from "./ethersRPC"; // for using ethers.js
+
+const clientId = "YOUR_CLIENT_ID"; // get from https://dashboard.web3auth.io
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent {
+    title = "angular-app";
+    web3auth: Web3Auth | null = null;
+    provider: SafeEventEmitterProvider | null = null;
+    isModalLoaded = false;
+
+    async ngOnInit() {
+      this.web3auth = new Web3Auth({
+        clientId,
+        chainConfig: {
+          chainNamespace: CHAIN_NAMESPACES.EIP155,
+          chainId: "0x1",
+          rpcTarget: "https://rpc.ankr.com/eth", // This is the public RPC we have added, please pass on your own endpoint while creating an app
+        },
+      });
+      const web3auth = this.web3auth
+
+
+      await web3auth.initModal();
+      if (web3auth.provider) {
+        this.provider = web3auth.provider;
+      }
+      this.isModalLoaded = true;
+    }
+
+    login = async () => {
+    if (!this.web3auth) {
+      console.log("web3auth not initialized yet");
+      return;
+    }
+    const web3auth = this.web3auth;
+    this.provider = await web3auth.connect();
+    console.log("logged in");
+    };
+
+    getUserInfo = async () => {
+      if (!this.web3auth) {
+        console.log("web3auth not initialized yet");
+        return;
+      }
+      const user = await this.web3auth.getUserInfo();
+      console.log(user);
+    };
+
+    getChainId = async () => {
+      if (!this.provider) {
+        console.log("provider not initialized yet");
+        return;
+      }
+      const rpc = new RPC(this.provider);
+      const chainId = await rpc.getChainId();
+      console.log(chainId);
+    };
+    getAccounts = async () => {
+      if (!this.provider) {
+        console.log("provider not initialized yet");
+        return;
+      }
+      const rpc = new RPC(this.provider);
+      const address = await rpc.getAccounts();
+      console.log(address);
+    };
+
+    getBalance = async () => {
+      if (!this.provider) {
+        console.log("provider not initialized yet");
+        return;
+      }
+      const rpc = new RPC(this.provider);
+      const balance = await rpc.getBalance();
+      console.log(balance);
+    };
+
+    sendTransaction = async () => {
+      if (!this.provider) {
+        console.log("provider not initialized yet");
+        return;
+      }
+      const rpc = new RPC(this.provider);
+      const receipt = await rpc.sendTransaction();
+      console.log(receipt);
+    };
+
+    signMessage = async () => {
+      if (!this.provider) {
+        console.log("provider not initialized yet");
+        return;
+      }
+      const rpc = new RPC(this.provider);
+      const signedMessage = await rpc.signMessage();
+      console.log(signedMessage);
+    };
+
+    getPrivateKey = async () => {
+      if (!this.provider) {
+        console.log("provider not initialized yet");
+        return;
+      }
+      const rpc = new RPC(this.provider);
+      const privateKey = await rpc.getPrivateKey();
+      console.log(privateKey);
+    };
+
+    logout = async () => {
+      if (!this.web3auth) {
+        console.log("web3auth not initialized yet");
+        return;
+      }
+      await this.web3auth.logout();
+      this.provider = null;
+      console.log("logged out");
+    };
+}
---
+++ web-modal-sdk/evm/commonjs-evm-web3auth-example/web3RPC.js
@@ -0,0 +1,93 @@
+const rpc = (() => {
+    /**
+     *
+     * @param {*} provider - provider received from Web3Auth login.
+     */
+  
+    const getChainId = async (provider) => {
+      const web3 = new Web3(provider);
+  
+      // Get the connected Chain's ID
+      const chainId = await web3.eth.getChainId();
+  
+      return chainId.toString();
+    };
+  
+    const getAccounts = async (provider) => {
+      const web3 = new Web3(provider);
+  
+      // Get user's Ethereum public address
+      const address = (await web3.eth.getAccounts())[0];
+  
+      return address;
+  
+    };
+  
+    const getBalance = async (provider) => {
+      const web3 = new Web3(provider);
+  
+      // Get user's Ethereum public address
+      const address = (await web3.eth.getAccounts())[0];
+  
+      // Get user's balance in ether
+      const balance = web3.utils.fromWei(
+        await web3.eth.getBalance(address) // Balance is in wei
+      );
+  
+      return balance;
+    };
+  
+    const sendTransaction = async (provider) => {
+      const web3 = new Web3(provider);
+  
+      // Get user's Ethereum public address
+      const address = (await web3.eth.getAccounts())[0];
+  
+      const destination = "0x7aFac68875d2841dc16F1730Fba43974060b907A";
+  
+      // Convert 1 ether to wei
+      const amount = web3.utils.toWei(0.001);
+  
+      // Submit transaction to the blockchain and wait for it to be mined
+      const receipt = await web3.eth.sendTransaction({
+        from: fromAddress,
+        to: destination,
+        value: amount,
+        maxPriorityFeePerGas: "5000000000", // Max priority fee per gas
+        maxFeePerGas: "6000000000000", // Max fee per gas
+      });
+  
+      return receipt;
+    }
+  
+    const signMessage = async (provider) => {
+      const web3 = new Web3(provider);
+  
+      // Get user's Ethereum public address
+      const fromAddress = (await web3.eth.getAccounts())[0];
+  
+      const originalMessage = "YOUR_MESSAGE";
+  
+      // Sign the message
+      const signedMessage = await web3.eth.personal.sign(originalMessage, fromAddress);
+  
+      return signedMessage;
+    };
+  
+    const getPrivateKey = async (provider) => {
+      const privateKey = await provider.request({
+        method: "eth_private_key",
+      });
+  
+      return privateKey;
+    }
+  
+    return {
+      getChainId,
+      getAccounts,
+      getBalance,
+      sendTransaction,
+      signMessage,
+      getPrivateKey
+    }
+  })()

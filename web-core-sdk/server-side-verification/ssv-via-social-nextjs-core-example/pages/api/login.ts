5e320263b86097ca203332c107f5dc941d232b44
Mohammad Shahbaz Alam <shahbaz17@users.noreply.github.com>
2022-08-18 15:57:08 +0530

    Update login.ts

--- web-core-sdk/server-side-verification/ssv-via-social-nextjs-core-example/pages/api/login.ts
+++ web-core-sdk/server-side-verification/ssv-via-social-nextjs-core-example/pages/api/login.ts
@@ -13,13 +13,13 @@ export default async function handler(
   try {
     const idToken = req.headers.authorization?.split(' ')[1] || ''
     const app_pub_key = req.body.appPubKey
-    
+
     const jwks = jose.createRemoteJWKSet(
       new URL('https://api.openlogin.com/jwks'),
     )
     const jwtDecoded = await jose.jwtVerify(idToken, jwks, {
       algorithms: ['ES256'],
-    });
+    })
     if ((jwtDecoded.payload as any).wallets[0].public_key == app_pub_key) {
       // Verified
       res.status(200).json({name: 'Validation Success'})

93a8b686c44e2ba7a28654b96da4e61877e2d843
Swaraj Phadtare <1swarajphadtare@gmail.com>
2022-08-18 15:33:02 +0530

    Fixed the approach to getting public key

--- web-core-sdk/server-side-verification/ssv-via-social-nextjs-core-example/pages/App.tsx
+++ web-core-sdk/server-side-verification/ssv-via-social-nextjs-core-example/pages/App.tsx
@@ -1,22 +1,16 @@
-import {useEffect, useState} from 'react'
-import {Web3AuthCore} from '@web3auth/core'
-import {
-  WALLET_ADAPTERS,
-  CHAIN_NAMESPACES,
-  SafeEventEmitterProvider,
-} from '@web3auth/base'
-import {OpenloginAdapter} from '@web3auth/openlogin-adapter'
+import { useEffect, useState } from "react";
+import { Web3AuthCore } from "@web3auth/core";
+import { WALLET_ADAPTERS, CHAIN_NAMESPACES, SafeEventEmitterProvider } from "@web3auth/base";
+import { OpenloginAdapter } from "@web3auth/openlogin-adapter";
 // import RPC from "./evm.web3";
-import RPC from './evm.ethers'
+import RPC from "./evm.ethers";
+import { getPublic, getPublicCompressed } from "@toruslabs/eccrypto";
 
-const clientId =
-  'BG7vMGIhzy7whDXXJPZ-JHme9haJ3PmV1-wl9SJPGGs9Cjk5_8m682DJ-lTDmwBWJe-bEHYE_t9gw0cdboLEwR8' // get from https://dashboard.web3auth.io
+const clientId = "BG7vMGIhzy7whDXXJPZ-JHme9haJ3PmV1-wl9SJPGGs9Cjk5_8m682DJ-lTDmwBWJe-bEHYE_t9gw0cdboLEwR8"; // get from https://dashboard.web3auth.io
 
 function App() {
-  const [web3auth, setWeb3auth] = useState<Web3AuthCore | null>(null)
-  const [provider, setProvider] = useState<SafeEventEmitterProvider | null>(
-    null,
-  )
+  const [web3auth, setWeb3auth] = useState<Web3AuthCore | null>(null);
+  const [provider, setProvider] = useState<SafeEventEmitterProvider | null>(null);
 
   useEffect(() => {
     const init = async () => {
@@ -24,142 +18,140 @@ function App() {
         const web3auth = new Web3AuthCore({
           chainConfig: {
             chainNamespace: CHAIN_NAMESPACES.EIP155,
-            chainId: '0x3',
-            rpcTarget:
-              'https://ropsten.infura.io/v3/0bb786dc49de43ce9b62a026c0297f9a',
+            chainId: "0x3",
+            rpcTarget: "https://ropsten.infura.io/v3/0bb786dc49de43ce9b62a026c0297f9a",
           },
-        })
+        });
 
         const openloginAdapter = new OpenloginAdapter({
           adapterSettings: {
             clientId,
-            network: 'testnet',
-            uxMode: 'popup',
+            network: "testnet",
+            uxMode: "popup",
             loginConfig: {
               google: {
-                name: 'Custom Google Auth Login',
-                verifier: 'web3auth-core-google',
-                typeOfLogin: 'google',
-                clientId:
-                  '774338308167-q463s7kpvja16l4l0kko3nb925ikds2p.apps.googleusercontent.com', //use your app client id you got from google
+                name: "Custom Google Auth Login",
+                verifier: "web3auth-core-google",
+                typeOfLogin: "google",
+                clientId: "774338308167-q463s7kpvja16l4l0kko3nb925ikds2p.apps.googleusercontent.com", //use your app client id you got from google
               },
             },
           },
-        })
-        web3auth.configureAdapter(openloginAdapter)
-        setWeb3auth(web3auth)
-
-        await web3auth.init()
+        });
+        web3auth.configureAdapter(openloginAdapter);
+        setWeb3auth(web3auth);
+        await web3auth.init();
         if (web3auth.provider) {
-          setProvider(web3auth.provider)
+          setProvider(web3auth.provider);
         }
       } catch (error) {
-        console.error(error)
+        console.error(error);
       }
-    }
+    };
 
-    init()
-  }, [])
+    init();
+  }, []);
 
   const login = async () => {
     if (!web3auth) {
-      uiConsole('web3auth not initialized yet')
-      return
+      uiConsole("web3auth not initialized yet");
+      return;
     }
-    const web3authProvider = await web3auth.connectTo(
-      WALLET_ADAPTERS.OPENLOGIN,
-      {
-        loginProvider: 'google',
-      },
-    )
-    setProvider(web3authProvider)
-  }
+    const web3authProvider = await web3auth.connectTo(WALLET_ADAPTERS.OPENLOGIN, {
+      loginProvider: "google",
+    });
+    setProvider(web3authProvider);
+  };
 
   const getUserInfo = async () => {
     if (!web3auth) {
-      uiConsole('web3auth not initialized yet')
-      return
+      uiConsole("web3auth not initialized yet");
+      return;
     }
-    const user = await web3auth.getUserInfo()
-    const parsedToken = parseJWT(user?.idToken as string)
+    const user = await web3auth.getUserInfo();
+
+    const privKey = await web3auth.provider?.request({
+      method: "eth_private_key",
+    });
+    const pubkey = getPublicCompressed(Buffer.from(privKey, "hex")).toString("hex");
 
     // Validate idToken with server
-    const res = await fetch('/api/login', {
-      method: 'POST',
+    const res = await fetch("/api/login", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        Authorization: 'Bearer ' + user.idToken,
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + user.idToken,
       },
-      body: JSON.stringify({appPubKey: parsedToken.wallets[0].public_key}),
-    })
+      body: JSON.stringify({ appPubKey: pubkey }),
+    });
     if (res.status === 200) {
-      console.log('JWT Verification Successful')
-      uiConsole(user)
+      console.log("JWT Verification Successful");
+      uiConsole(user);
     } else {
-      console.log('JWT Verification Failed')
-      uiConsole('JWT Verification Failed')
+      console.log("JWT Verification Failed");
+      uiConsole("JWT Verification Failed");
     }
-  }
+  };
 
   const parseJWT = (token: string) => {
-    const base64Url = token.split('.')[1]
-    const base64 = base64Url.replace('-', '+').replace('_', '/')
-    return JSON.parse(window.atob(base64))
-  }
+    const base64Url = token.split(".")[1];
+    const base64 = base64Url.replace("-", "+").replace("_", "/");
+    return JSON.parse(window.atob(base64));
+  };
 
   const logout = async () => {
     if (!web3auth) {
-      uiConsole('web3auth not initialized yet')
-      return
+      uiConsole("web3auth not initialized yet");
+      return;
     }
-    await web3auth.logout()
-    setProvider(null)
-  }
+    await web3auth.logout();
+    setProvider(null);
+  };
 
   const getAccounts = async () => {
     if (!provider) {
-      uiConsole('provider not initialized yet')
-      return
+      uiConsole("provider not initialized yet");
+      return;
     }
-    const rpc = new RPC(provider)
-    const userAccount = await rpc.getAccounts()
-    uiConsole(userAccount)
-  }
+    const rpc = new RPC(provider);
+    const userAccount = await rpc.getAccounts();
+    uiConsole(userAccount);
+  };
 
   const getBalance = async () => {
     if (!provider) {
-      uiConsole('provider not initialized yet')
-      return
+      uiConsole("provider not initialized yet");
+      return;
     }
-    const rpc = new RPC(provider)
-    const balance = await rpc.getBalance()
-    uiConsole(balance)
-  }
+    const rpc = new RPC(provider);
+    const balance = await rpc.getBalance();
+    uiConsole(balance);
+  };
 
   const signMessage = async () => {
     if (!provider) {
-      uiConsole('provider not initialized yet')
-      return
+      uiConsole("provider not initialized yet");
+      return;
     }
-    const rpc = new RPC(provider)
-    const result = await rpc.signMessage()
-    uiConsole(result)
-  }
+    const rpc = new RPC(provider);
+    const result = await rpc.signMessage();
+    uiConsole(result);
+  };
 
   const sendTransaction = async () => {
     if (!provider) {
-      uiConsole('provider not initialized yet')
-      return
+      uiConsole("provider not initialized yet");
+      return;
     }
-    const rpc = new RPC(provider)
-    const result = await rpc.signAndSendTransaction()
-    uiConsole(result)
-  }
+    const rpc = new RPC(provider);
+    const result = await rpc.signAndSendTransaction();
+    uiConsole(result);
+  };
 
   function uiConsole(...args: any[]): void {
-    const el = document.querySelector('#console>p')
+    const el = document.querySelector("#console>p");
     if (el) {
-      el.innerHTML = JSON.stringify(args || {}, null, 2)
+      el.innerHTML = JSON.stringify(args || {}, null, 2);
     }
   }
 
@@ -198,47 +190,39 @@ function App() {
         </div>
       </div>
 
-      <div id="console" style={{whiteSpace: 'pre-line'}}>
-        <p style={{whiteSpace: 'pre-line'}}></p>
+      <div id="console" style={{ whiteSpace: "pre-line" }}>
+        <p style={{ whiteSpace: "pre-line" }}></p>
       </div>
     </>
-  )
+  );
 
   const logoutView = (
     <button onClick={login} className="card">
       Login
     </button>
-  )
+  );
 
   return (
     <div className="container">
       <h1 className="title">
         <a target="_blank" href="http://web3auth.io/" rel="noreferrer">
           Web3Auth
-        </a>{' '}
+        </a>{" "}
         & NextJS Example for Google Login
       </h1>
 
       <div className="grid">{provider ? loginView : logoutView}</div>
 
       <footer className="footer">
-        <a
-          href="https://github.com/Web3Auth/examples/tree/master/google-core-react-example"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://github.com/Web3Auth/examples/tree/master/google-core-react-example" target="_blank" rel="noopener noreferrer">
           Source code
         </a>
-        <a
-          href="https://faucet.egorfine.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://faucet.egorfine.com/" target="_blank" rel="noopener noreferrer">
           Ropsten Faucet
         </a>
       </footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

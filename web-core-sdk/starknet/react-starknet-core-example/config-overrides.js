0a20be10736f4100ef5f2471e236c493478cdb6c
Saviour1001 <harshghodkar@gmail.com>
2022-11-02 21:05:55 +0530

    Updated the packages and added the new web pack fix to all examples

--- web-core-sdk/starknet/react-starknet-core-example/config-overrides.js
+++ web-core-sdk/starknet/react-starknet-core-example/config-overrides.js
@@ -1,35 +1,31 @@
 const webpack = require("webpack");
 
 module.exports = function override(config) {
-    const fallback = config.resolve.fallback || {};
-    Object.assign(fallback, {
-        crypto: require.resolve("crypto-browserify"),
-        stream: require.resolve("stream-browserify"),
-        assert: require.resolve("assert"),
-        http: require.resolve("stream-http"),
-        https: require.resolve("https-browserify"),
-        os: require.resolve("os-browserify"),
-        url: require.resolve("url"),
-    });
-    config.resolve.fallback = fallback;
-    config.plugins = (config.plugins || []).concat([
-        new webpack.ProvidePlugin({
-            process: "process/browser",
-            Buffer: ["buffer", "Buffer"],
-        }),
-    ]);
-    config.ignoreWarnings = [/Failed to parse source map/];
-    config.module.rules = config.module.rules.map(rule => {
-        if (rule.oneOf instanceof Array) {
-            rule.oneOf[rule.oneOf.length - 1].exclude = [/\.(js|mjs|jsx|cjs|ts|tsx)$/, /\.html$/, /\.json$/];
-        }
-        return rule;
-    });
-    config.module.rules.unshift({
-        test: /\.m?js$/,
-        resolve: {
-          fullySpecified: false, // disable the behavior
-        },
-      });
-    return config;
+  const fallback = config.resolve.fallback || {};
+  Object.assign(fallback, {
+    crypto: require.resolve("crypto-browserify"),
+    stream: require.resolve("stream-browserify"),
+    assert: require.resolve("assert"),
+    http: require.resolve("stream-http"),
+    https: require.resolve("https-browserify"),
+    os: require.resolve("os-browserify"),
+    url: require.resolve("url"),
+  });
+  config.resolve.fallback = fallback;
+  config.plugins = (config.plugins || []).concat([
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+      Buffer: ["buffer", "Buffer"],
+    }),
+  ]);
+  config.ignoreWarnings = [/Failed to parse source map/];
+  config.module.rules.push({
+    test: /\.(js|mjs|jsx)$/,
+    enforce: "pre",
+    loader: require.resolve("source-map-loader"),
+    resolve: {
+      fullySpecified: false,
+    },
+  });
+  return config;
 };

5f589a8fef66b8e0e827143da30ad30d814bfcd1
Saviour1001 <harshghodkar@gmail.com>
2022-11-05 14:52:47 +0530

    Added the example

---
+++ nodejs-sdk/react-firebase-app/src/App.tsx
@@ -0,0 +1,234 @@
+import { useEffect, useState } from "react";
+import { Web3Auth } from "@web3auth/node-sdk";
+import { SafeEventEmitterProvider } from "@web3auth/base";
+import "./App.css";
+import RPC from "./web3RPC"; // for using web3.js
+import { initializeApp } from "firebase/app";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  UserCredential,
+} from "firebase/auth";
+
+const clientId =
+  "BBP_6GOu3EJGGws9yd8wY_xFT0jZIWmiLMpqrEMx36jlM61K9XRnNLnnvEtGpF-RhXJDGMJjL-I-wTi13RcBBOo"; // get from https://dashboard.web3auth.io
+
+const verifier = "web3auth-core-firebase";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB0nd9YsPLu-tpdCrsXn8wgsWVAiYEpQ_E",
+  authDomain: "web3auth-oauth-logins.firebaseapp.com",
+  projectId: "web3auth-oauth-logins",
+  storageBucket: "web3auth-oauth-logins.appspot.com",
+  messagingSenderId: "461819774167",
+  appId: "1:461819774167:web:e74addfb6cc88f3b5b9c92",
+};
+
+function App() {
+  const [web3auth, setWeb3auth] = useState<Web3Auth | null>(null);
+  const [provider, setProvider] = useState<SafeEventEmitterProvider | null>(
+    null
+  );
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        // ETH_Ropsten
+        const web3auth = new Web3Auth({
+          clientId, // Get your Client ID from Web3Auth Dashboard
+          chainConfig: {
+            chainNamespace: "eip155",
+            chainId: "0x1",
+            rpcTarget: "https://rpc.ankr.com/eth", // needed for non-other chains
+          },
+        });
+
+        setWeb3auth(web3auth);
+
+        await web3auth.init({
+          network: "testnet",
+        });
+
+        if (web3auth.provider) {
+          setProvider(web3auth.provider);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    init();
+  }, []);
+
+  const signInWithGoogle = async (): Promise<UserCredential> => {
+    try {
+      const app = initializeApp(firebaseConfig);
+      const auth = getAuth(app);
+      const googleProvider = new GoogleAuthProvider();
+      const res = await signInWithPopup(auth, googleProvider);
+      console.log(res);
+      return res;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  };
+
+  const parseToken = (token: any) => {
+    try {
+      const base64Url = token?.split(".")[1];
+      const base64 = base64Url?.replace("-", "+").replace("_", "/");
+      return JSON.parse(window.atob(base64 || ""));
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  };
+
+  const login = async () => {
+    if (!web3auth) {
+      uiConsole("web3auth not initialized yet");
+      return;
+    }
+
+    // login with firebase
+    const loginRes = await signInWithGoogle();
+    // get the id token from firebase
+    const idToken = await loginRes.user.getIdToken(true);
+
+    // get sub value from firebase id token
+    const { sub } = parseToken(idToken);
+
+    const web3authProvider = await web3auth.connect({
+      verifier: verifier, // replace with your verifier name
+      verifierId: sub, // replace with your verifier id, setup while creating the verifier on Web3Auth's Dashboard
+      idToken: idToken,
+    });
+    setProvider(web3authProvider);
+  };
+
+  const logout = async () => {
+    if (!web3auth) {
+      uiConsole("web3auth not initialized yet");
+      return;
+    }
+    console.log("logout");
+    // setProvider(null);
+  };
+
+  const getAccounts = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const address = await rpc.getAccounts();
+    uiConsole("ETH Address: " + address);
+  };
+
+  const getBalance = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const balance = await rpc.getBalance();
+    uiConsole(balance);
+  };
+
+  const sendTransaction = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const receipt = await rpc.sendTransaction();
+    uiConsole(receipt);
+  };
+
+  const signMessage = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const signedMessage = await rpc.signMessage();
+    uiConsole(signedMessage);
+  };
+
+  function uiConsole(...args: any[]): void {
+    const el = document.querySelector("#console>p");
+    if (el) {
+      el.innerHTML = JSON.stringify(args || {}, null, 2);
+    }
+  }
+
+  const loggedInView = (
+    <>
+      <div className="flex-container">
+        <div>
+          <button onClick={getAccounts} className="card">
+            Get ETH Account
+          </button>
+        </div>
+
+        <div>
+          <button onClick={getBalance} className="card">
+            Get Balance
+          </button>
+        </div>
+        <div>
+          <button onClick={sendTransaction} className="card">
+            Send Transaction
+          </button>
+        </div>
+        <div>
+          <button onClick={signMessage} className="card">
+            Sign Message
+          </button>
+        </div>
+        <div>
+          <button onClick={logout} className="card">
+            Log Out
+          </button>
+        </div>
+      </div>
+      <div id="console" style={{ whiteSpace: "pre-line" }}>
+        <p style={{ whiteSpace: "pre-line" }}></p>
+      </div>
+    </>
+  );
+
+  const unloggedInView = (
+    <button onClick={login} className="card">
+      Login
+    </button>
+  );
+
+  return (
+    <div className="container">
+      <h1 className="title">
+        <a target="_blank" href="http://web3auth.io/" rel="noreferrer">
+          Web3Auth{" "}
+        </a>
+        & ReactJS Example
+      </h1>
+
+      <div className="grid">{provider ? loggedInView : unloggedInView}</div>
+
+      <footer className="footer">
+        <a
+          href="https://github.com/Web3Auth/examples/tree/main/web-modal-sdk/multi-chain"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Source code
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;

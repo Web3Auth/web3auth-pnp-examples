a6bb6c0f995d677216f3c02059cef0107f5c8d0d
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2022-11-06 11:55:11 +0530

    ios firebase setup

--- react-native/web3auth-rn-bare-firebase-example/ios/web3authrnbarefirebase/AppDelegate.mm
+++ react-native/web3auth-rn-bare-firebase-example/ios/web3authrnbarefirebase/AppDelegate.mm
@@ -1,4 +1,5 @@
 #import "AppDelegate.h"
+#import <Firebase.h>
 
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
@@ -32,6 +33,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   RCTAppSetupPrepareApp(application);
+  [FIRApp configure];
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
 

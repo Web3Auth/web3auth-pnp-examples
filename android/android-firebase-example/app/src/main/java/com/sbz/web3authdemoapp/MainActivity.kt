9f255c6c33c279239a965f52eef737e67b8788ba
Mohammad Shahbaz Alam <shahbaz17@users.noreply.github.com>
2022-11-08 23:11:35 +0530

    Add Android Firebase Example

---
+++ android/android-firebase-example/app/src/main/java/com/sbz/web3authdemoapp/MainActivity.kt
@@ -0,0 +1,176 @@
+package com.sbz.web3authdemoapp
+
+import android.content.ContentValues.TAG
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.google.gson.Gson
+import com.web3auth.core.Web3Auth
+import com.web3auth.core.types.*
+import java8.util.concurrent.CompletableFuture
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var web3Auth: Web3Auth
+
+    private lateinit var auth: FirebaseAuth
+
+    private val gson = Gson()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        web3Auth = Web3Auth(
+           Web3AuthOptions(
+               context = this,
+               clientId = getString(R.string.web3auth_project_id), // pass over your Web3Auth Client ID from Developer Dashboard
+               network = Web3Auth.Network.TESTNET, // pass over the network you want to use (MAINNET or TESTNET or CYAN)
+               redirectUrl = Uri.parse("com.sbz.web3authdemoapp://auth"), // your app's redirect URL
+               // Optional parameters
+               whiteLabel = WhiteLabelData(
+                   "Web3Auth Android Firebase Example", null, null, "en", true,
+                   hashMapOf(
+                       "primary" to "#F5820D"
+                   )
+               ),
+               loginConfig = hashMapOf("jwt" to LoginConfigItem(
+                   verifier = "web3auth-core-firebase",
+                   typeOfLogin = TypeOfLogin.JWT,
+                   name = "Firebase Login",
+                   clientId = getString(R.string.web3auth_project_id)
+               ))
+           )
+       )
+
+        // Handle user signing in when app is not alive
+        web3Auth.setResultUrl(intent?.data)
+
+        // Call sessionResponse() in onCreate() to check for any existing session.
+        val sessionResponse: CompletableFuture<Web3AuthResponse> = web3Auth.sessionResponse()
+        sessionResponse.whenComplete { loginResponse, error ->
+            if (error == null) {
+                println(loginResponse)
+                reRender(loginResponse)
+            } else {
+                Log.d("MainActivity_Web3Auth", error.message ?: "Something went wrong")
+                // Ideally, you should initiate the login function here.
+            }
+        }
+
+        // Setup UI and event handlers
+        val signInButton = findViewById<Button>(R.id.signInButton)
+        signInButton.setOnClickListener { signIn() }
+
+        val signOutButton = findViewById<Button>(R.id.signOutButton)
+        signOutButton.setOnClickListener { signOut() }
+
+        reRender(Web3AuthResponse())
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        // Handle user signing in when app is active
+        web3Auth.setResultUrl(intent?.data)
+    }
+
+    private fun signIn() {
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        auth.signInWithEmailAndPassword("android@firebase.com", "Android@Web3Auth")
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    user!!.getIdToken(true).addOnSuccessListener { result ->
+                        val idToken = result.token
+                        //Do whatever
+                        Log.d(TAG, "GetTokenResult result = $idToken")
+                        val selectedLoginProvider = Provider.JWT
+                        val loginCompletableFuture: CompletableFuture<Web3AuthResponse> = web3Auth.login(LoginParams(selectedLoginProvider, extraLoginOptions = ExtraLoginOptions(domain = "firebase", id_token = idToken)))
+
+                        loginCompletableFuture.whenComplete { loginResponse, error ->
+                            if (error == null) {
+                                println(loginResponse)
+                                reRender(loginResponse)
+                            } else {
+                                Log.d("MainActivity_Web3Auth", error.message ?: "Something went wrong" )
+                            }
+                        }
+                    }
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+
+                }
+            }
+
+//        val selectedLoginProvider = Provider.JWT   // Can be GOOGLE, FACEBOOK, TWITCH etc.
+//        val loginCompletableFuture: CompletableFuture<Web3AuthResponse> = web3Auth.login(LoginParams(selectedLoginProvider, extraLoginOptions = ExtraLoginOptions(domain = "firebase", id_token = "token")))
+
+    //    For Email Passwordless, use the below code and pass email id into extraLoginOptions of LoginParams.
+    //    val selectedLoginProvider = Provider.EMAIL_PASSWORDLESS
+    //    val loginCompletableFuture: CompletableFuture<Web3AuthResponse> = web3Auth.login(LoginParams(selectedLoginProvider, extraLoginOptions = ExtraLoginOptions(login_hint = "shahbaz.web3@gmail.com")))
+
+    //    For login with Custom JWT, use the below code and pass email id into extraLoginOptions of LoginParams.
+    //    val selectedLoginProvider = Provider.JWT
+    //    val loginCompletableFuture: CompletableFuture<Web3AuthResponse> = web3Auth.login(LoginParams(selectedLoginProvider, extraLoginOptions = ExtraLoginOptions(id_token = "<id-token>", domain: "your-domain")))
+
+//        loginCompletableFuture.whenComplete { loginResponse, error ->
+//            if (error == null) {
+//                println(loginResponse)
+//                reRender(loginResponse)
+//            } else {
+//                Log.d("MainActivity_Web3Auth", error.message ?: "Something went wrong" )
+//            }
+//        }
+    }
+
+    private fun signOut() {
+        val logoutCompletableFuture =  web3Auth.logout()
+        logoutCompletableFuture.whenComplete { _, error ->
+            if (error == null) {
+                reRender(Web3AuthResponse())
+            } else {
+                Log.d("MainActivity_Web3Auth", error.message ?: "Something went wrong" )
+            }
+        }
+        recreate()
+    }
+
+    private fun reRender(web3AuthResponse: Web3AuthResponse) {
+        val contentTextView = findViewById<TextView>(R.id.contentTextView)
+        val signInButton = findViewById<Button>(R.id.signInButton)
+        val signOutButton = findViewById<Button>(R.id.signOutButton)
+
+        val key = web3AuthResponse.privKey
+        val userInfo = web3AuthResponse.userInfo
+        println(userInfo)
+        if (key is String && key.isNotEmpty()) {
+            contentTextView.text = gson.toJson(web3AuthResponse)
+            contentTextView.visibility = View.VISIBLE
+            signInButton.visibility = View.GONE
+            signOutButton.visibility = View.VISIBLE
+        } else {
+            contentTextView.text = getString(R.string.not_logged_in)
+            contentTextView.visibility = View.GONE
+            signInButton.visibility = View.VISIBLE
+            signOutButton.visibility = View.GONE
+        }
+    }
+}

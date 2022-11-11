07bee769821980ba6983c576b63e7543110d07ac
Mohammad Shahbaz Alam <shahbaz17@users.noreply.github.com>
2022-11-11 12:03:42 +0530

    add authenticateUser() + ui fixes to Vue.js Solana Example

--- web-modal-sdk/solana/vue-solana-web3auth-example/src/Home.vue
+++ web-modal-sdk/solana/vue-solana-web3auth-example/src/Home.vue
@@ -1,203 +1,299 @@
 <template>
-  <div id="app">
-    <h2>Web3Auth X Vue.js</h2>
-    <section
-      style="
-         {
-          fontsize: '12px';
-        }
-      "
-    >
-      <button class="rpcBtn" @click="login" style="cursor: pointer">
-        Login
-      </button>
-      <button class="rpcBtn" @click="getUserInfo" style="cursor: pointer">
-        Get User Info
-      </button>
-      <button class="rpcBtn" @click="getAccounts" style="cursor: pointer">
-        Get Accounts
-      </button>
-      <button class="rpcBtn" @click="getBalance" style="cursor: pointer">
-        Get Balance
-      </button>
-      <button class="rpcBtn" @click="sendTransaction" style="cursor: pointer">
-        Send Transaction
-      </button>
-      <button class="rpcBtn" @click="signMessage" style="cursor: pointer">
-        Sign Message
-      </button>
-      <button class="rpcBtn" @click="getPrivateKey" style="cursor: pointer">
-        Get Private Key
-      </button>
-      <button class="rpcBtn" @click="logout" style="cursor: pointer">
-        Logout
-      </button>
-    </section>
-    <div id="console" style="white-space: pre-line">
-      <p style="white-space: pre-line"></p>
-    </div>
-  </div>
+	<div id="app">
+		<h2>
+			<a target="_blank" href="http://web3auth.io/" rel="noreferrer">
+				Web3Auth
+			</a>
+			Vue.js Solana Example
+		</h2>
+
+		<button
+			v-if="!loggedin"
+			class="card"
+			@click="login"
+			style="cursor: pointer"
+		>
+			Login
+		</button>
+
+		<div v-if="loggedin">
+			<div class="flex-container">
+				<div>
+					<button class="card" @click="getUserInfo" style="cursor: pointer">
+						Get User Info
+					</button>
+				</div>
+				<div>
+					<button
+						class="card"
+						@click="authenticateUser"
+						style="cursor: pointer"
+					>
+						Get idToken
+					</button>
+				</div>
+				<div>
+					<button class="card" @click="getAccounts" style="cursor: pointer">
+						Get Accounts
+					</button>
+				</div>
+				<div>
+					<button class="card" @click="getBalance" style="cursor: pointer">
+						Get Balance
+					</button>
+				</div>
+				<div>
+					<button class="card" @click="sendTransaction" style="cursor: pointer">
+						Send Transaction
+					</button>
+				</div>
+				<div>
+					<button class="card" @click="signMessage" style="cursor: pointer">
+						Sign Message
+					</button>
+				</div>
+				<div>
+					<button class="card" @click="getPrivateKey" style="cursor: pointer">
+						Get Private Key
+					</button>
+				</div>
+				<div>
+					<button class="card" @click="logout" style="cursor: pointer">
+						Logout
+					</button>
+				</div>
+			</div>
+			<div id="console" style="white-space: pre-line">
+				<p style="white-space: pre-line"></p>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
-import { ref, onMounted } from "vue";
-import { Web3Auth } from "@web3auth/modal";
-import { CHAIN_NAMESPACES, SafeEventEmitterProvider } from "@web3auth/base";
-import RPC from "./solanaRPC";
+import { ref, onMounted } from 'vue';
+import { Web3Auth } from '@web3auth/modal';
+import { CHAIN_NAMESPACES, SafeEventEmitterProvider } from '@web3auth/base';
+import RPC from './solanaRPC';
 
 export default {
-  name: "Home",
-  props: {
-    msg: String,
-  },
-  setup() {
-    const loading = ref<boolean>(false);
-    const loginButtonStatus = ref<string>("");
-    const connecting = ref<boolean>(false);
-    let provider = ref<SafeEventEmitterProvider | any>(null);
-    const clientId = "BHr_dKcxC0ecKn_2dZQmQeNdjPgWykMkcodEHkVvPMo71qzOV6SgtoN8KCvFdLN7bf34JOm89vWQMLFmSfIo84A"; // get from https://dashboard.web3auth.io
+	name: 'Home',
+	props: {
+		msg: String,
+	},
+	setup() {
+		const loggedin = ref<boolean>(false);
+		const loading = ref<boolean>(false);
+		const loginButtonStatus = ref<string>('');
+		const connecting = ref<boolean>(false);
+		let provider = ref<SafeEventEmitterProvider | any>(null);
+		const clientId =
+			'BHr_dKcxC0ecKn_2dZQmQeNdjPgWykMkcodEHkVvPMo71qzOV6SgtoN8KCvFdLN7bf34JOm89vWQMLFmSfIo84A'; // get from https://dashboard.web3auth.io
 
-    const web3auth = new Web3Auth({
-      clientId,
-      chainConfig: {
-        chainNamespace: CHAIN_NAMESPACES.SOLANA,
-        chainId: "0x1", // Please use 0x1 for Mainnet, 0x2 for Testnet, 0x3 for Devnet
-        rpcTarget: "https://api.mainnet-beta.solana.com", // This is the public RPC we have added, please pass on your own endpoint while creating an app
-      },
-    });
+		const web3auth = new Web3Auth({
+			clientId,
+			chainConfig: {
+				chainNamespace: CHAIN_NAMESPACES.SOLANA,
+				chainId: '0x1', // Please use 0x1 for Mainnet, 0x2 for Testnet, 0x3 for Devnet
+				rpcTarget: 'https://api.mainnet-beta.solana.com', // This is the public RPC we have added, please pass on your own endpoint while creating an app
+			},
+		});
 
-    onMounted(async () => {
-      try {
-        loading.value = true;
+		onMounted(async () => {
+			try {
+				loading.value = true;
+				loggedin.value = false;
 
-        await web3auth.initModal();
-        if (web3auth.provider) {
-          provider = web3auth.provider;
-        }
-      } catch (error) {
-        uiConsole("error", error);
-        uiConsole("error", error);
-      } finally {
-        loading.value = false;
-      }
-    });
+				await web3auth.initModal();
+				if (web3auth.provider) {
+					provider = web3auth.provider;
+					loggedin.value = true;
+				}
+			} catch (error) {
+				uiConsole('error', error);
+				uiConsole('error', error);
+			} finally {
+				loading.value = false;
+			}
+		});
 
-    const login = async () => {
-      if (!web3auth) {
-        uiConsole("web3auth not initialized yet");
-        return;
-      }
-      provider = await web3auth.connect();
-    };
+		const login = async () => {
+			if (!web3auth) {
+				uiConsole('web3auth not initialized yet');
+				return;
+			}
+			provider = await web3auth.connect();
+			loggedin.value = true;
+		};
 
-    const getUserInfo = async () => {
-      if (!web3auth) {
-        uiConsole("web3auth not initialized yet");
-        return;
-      }
-      const user = await web3auth.getUserInfo();
-      uiConsole(user);
-    };
+		const authenticateUser = async () => {
+			if (!web3auth) {
+				uiConsole('web3auth not initialized yet');
+				return;
+			}
+			const idToken = await web3auth.authenticateUser();
+			uiConsole(idToken);
+		};
 
-    const logout = async () => {
-      if (!web3auth) {
-        uiConsole("web3auth not initialized yet");
-        return;
-      }
-      await web3auth.logout();
-      provider = null;
-    };
+		const getUserInfo = async () => {
+			if (!web3auth) {
+				uiConsole('web3auth not initialized yet');
+				return;
+			}
+			const user = await web3auth.getUserInfo();
+			uiConsole(user);
+		};
 
-    const getAccounts = async () => {
-      if (!provider) {
-        uiConsole("provider not initialized yet");
-        return;
-      }
-      const rpc = new RPC(provider);
-      const address = await rpc.getAccounts();
-      uiConsole(address);
-    };
+		const logout = async () => {
+			if (!web3auth) {
+				uiConsole('web3auth not initialized yet');
+				return;
+			}
+			await web3auth.logout();
+			provider = null;
+			loggedin.value = false;
+		};
 
-    const getBalance = async () => {
-      if (!provider) {
-        uiConsole("provider not initialized yet");
-        return;
-      }
-      const rpc = new RPC(provider);
-      const balance = await rpc.getBalance();
-      uiConsole(balance);
-    };
+		const getAccounts = async () => {
+			if (!provider) {
+				uiConsole('provider not initialized yet');
+				return;
+			}
+			const rpc = new RPC(provider);
+			const address = await rpc.getAccounts();
+			uiConsole(address);
+		};
 
-    const sendTransaction = async () => {
-      if (!provider) {
-        uiConsole("provider not initialized yet");
-        return;
-      }
-      const rpc = new RPC(provider);
-      const receipt = await rpc.sendTransaction();
-      uiConsole(receipt);
-    };
+		const getBalance = async () => {
+			if (!provider) {
+				uiConsole('provider not initialized yet');
+				return;
+			}
+			const rpc = new RPC(provider);
+			const balance = await rpc.getBalance();
+			uiConsole(balance);
+		};
 
-    const signMessage = async () => {
-      if (!provider) {
-        uiConsole("provider not initialized yet");
-        return;
-      }
-      const rpc = new RPC(provider);
-      const signedMessage = await rpc.signMessage();
-      uiConsole(signedMessage);
-    };
+		const sendTransaction = async () => {
+			if (!provider) {
+				uiConsole('provider not initialized yet');
+				return;
+			}
+			const rpc = new RPC(provider);
+			const receipt = await rpc.sendTransaction();
+			uiConsole(receipt);
+		};
 
-    const getPrivateKey = async () => {
-      if (!provider) {
-        uiConsole("provider not initialized yet");
-        return;
-      }
-      const rpc = new RPC(provider);
-      const privateKey = await rpc.getPrivateKey();
-      uiConsole(privateKey);
-    };
+		const signMessage = async () => {
+			if (!provider) {
+				uiConsole('provider not initialized yet');
+				return;
+			}
+			const rpc = new RPC(provider);
+			const signedMessage = await rpc.signMessage();
+			uiConsole(signedMessage);
+		};
 
-    function uiConsole(...args: any[]): void {
-      const el = document.querySelector("#console>p")
-      if (el) {
-        el.innerHTML = JSON.stringify(args || {}, null, 2)
-      }
-    }
+		const getPrivateKey = async () => {
+			if (!provider) {
+				uiConsole('provider not initialized yet');
+				return;
+			}
+			const rpc = new RPC(provider);
+			const privateKey = await rpc.getPrivateKey();
+			uiConsole(privateKey);
+		};
 
-    return {
-      loading,
-      loginButtonStatus,
-      connecting,
-      provider,
-      web3auth,
-      login,
-      logout,
-      getUserInfo,
-      getAccounts,
-      getBalance,
-      sendTransaction,
-      signMessage,
-      getPrivateKey,
-    };
-  },
+		function uiConsole(...args: any[]): void {
+			const el = document.querySelector('#console>p');
+			if (el) {
+				el.innerHTML = JSON.stringify(args || {}, null, 2);
+			}
+		}
+
+		return {
+			loggedin,
+			loading,
+			loginButtonStatus,
+			connecting,
+			provider,
+			web3auth,
+			login,
+			authenticateUser,
+			logout,
+			getUserInfo,
+			getAccounts,
+			getBalance,
+			sendTransaction,
+			signMessage,
+			getPrivateKey,
+		};
+	},
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#app {
+	width: 70%;
+	margin: auto;
+	padding: 0 2rem;
+}
 h3 {
-  margin: 40px 0 0;
+	margin: 40px 0 0;
 }
 ul {
-  list-style-type: none;
-  padding: 0;
+	list-style-type: none;
+	padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+	display: inline-block;
+	margin: 0 10px;
 }
 a {
-  color: #42b983;
+	color: #42b983;
+}
+
+.card {
+	margin: 0.5rem;
+	padding: 0.7rem;
+	text-align: center;
+	color: #0070f3;
+	background-color: #fafafa;
+	text-decoration: none;
+	border: 1px solid #0070f3;
+	border-radius: 10px;
+	transition: color 0.15s ease, border-color 0.15s ease;
+	width: 100%;
+}
+
+.card:hover,
+.card:focus,
+.card:active {
+	cursor: pointer;
+	background-color: #f1f1f1;
+}
+
+.flex-container {
+	display: flex;
+	flex-flow: row wrap;
+}
+
+.flex-container > div {
+	width: 100px;
+	margin: 10px;
+	text-align: center;
+	line-height: 75px;
+	font-size: 30px;
+}
+
+#console {
+	width: 100%;
+	height: 100%;
+	overflow: auto;
+	word-wrap: break-word;
+	font-size: 16px;
+	font-family: monospace;
+	text-align: left;
 }
 </style>

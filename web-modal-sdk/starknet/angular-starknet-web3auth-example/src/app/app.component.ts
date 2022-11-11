b65852fe837aa03cac93305d198fefc439664120
Mohammad Shahbaz Alam <shahbaz17@users.noreply.github.com>
2022-11-11 18:37:17 +0530

    add authenticateUser() to StarkNet example

--- web-modal-sdk/starknet/angular-starknet-web3auth-example/src/app/app.component.ts
+++ web-modal-sdk/starknet/angular-starknet-web3auth-example/src/app/app.component.ts
@@ -1,113 +1,123 @@
 import { Component } from '@angular/core';
 import { Web3Auth } from '@web3auth/modal';
 import {
-  WALLET_ADAPTERS,
-  CHAIN_NAMESPACES,
-  SafeEventEmitterProvider,
+	WALLET_ADAPTERS,
+	CHAIN_NAMESPACES,
+	SafeEventEmitterProvider,
 } from '@web3auth/base';
 import { OpenloginAdapter } from '@web3auth/openlogin-adapter';
 import RPC from './starknetRPC';
-const clientId = 'BHr_dKcxC0ecKn_2dZQmQeNdjPgWykMkcodEHkVvPMo71qzOV6SgtoN8KCvFdLN7bf34JOm89vWQMLFmSfIo84A'; // get from https://dashboard.web3auth.io
+const clientId =
+	'BHr_dKcxC0ecKn_2dZQmQeNdjPgWykMkcodEHkVvPMo71qzOV6SgtoN8KCvFdLN7bf34JOm89vWQMLFmSfIo84A'; // get from https://dashboard.web3auth.io
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-app';
-  web3auth: Web3Auth | null = null;
-  provider: SafeEventEmitterProvider | null = null;
-  isModalLoaded = false;
+	title = 'angular-app';
+	web3auth: Web3Auth | null = null;
+	provider: SafeEventEmitterProvider | null = null;
+	isModalLoaded = false;
 
-  async ngOnInit() {
-    this.web3auth = new Web3Auth({
-      clientId,
-      chainConfig: {
-        chainNamespace: CHAIN_NAMESPACES.OTHER,
-      },
-    });
-    const web3auth = this.web3auth;
+	async ngOnInit() {
+		this.web3auth = new Web3Auth({
+			clientId,
+			chainConfig: {
+				chainNamespace: CHAIN_NAMESPACES.OTHER,
+			},
+		});
+		const web3auth = this.web3auth;
 
-    const openloginAdapter = new OpenloginAdapter({
-      adapterSettings: {
-        clientId,
-        network: 'testnet',
-        uxMode: 'popup',
-      },
-    });
-    web3auth.configureAdapter(openloginAdapter);
+		const openloginAdapter = new OpenloginAdapter({
+			adapterSettings: {
+				clientId,
+				network: 'testnet',
+				uxMode: 'popup',
+			},
+		});
+		web3auth.configureAdapter(openloginAdapter);
 
-    await web3auth.initModal();
-    if (web3auth.provider) {
-      this.provider = web3auth.provider;
-    }
-    this.isModalLoaded = true;
-  }
+		await web3auth.initModal();
+		if (web3auth.provider) {
+			this.provider = web3auth.provider;
+		}
+		this.isModalLoaded = true;
+	}
 
-  login = async () => {
-    if (!this.web3auth) {
-      this.uiConsole('web3auth not initialized yet');
-      return;
-    }
-    const web3auth = this.web3auth;
-    this.provider = await web3auth.connect();
-    this.uiConsole('logged in');
-  };
+	login = async () => {
+		if (!this.web3auth) {
+			this.uiConsole('web3auth not initialized yet');
+			return;
+		}
+		const web3auth = this.web3auth;
+		this.provider = await web3auth.connect();
+		this.uiConsole('logged in');
+	};
 
-  getUserInfo = async () => {
-    if (!this.web3auth) {
-      this.uiConsole('web3auth not initialized yet');
-      return;
-    }
-    const user = await this.web3auth.getUserInfo();
-    this.uiConsole(user);
-  };
+	authenticateUser = async () => {
+		if (!this.web3auth) {
+			this.uiConsole('web3auth not initialized yet');
+			return;
+		}
+		const id_token = await this.web3auth.authenticateUser();
+		this.uiConsole(id_token);
+	};
 
-  onGetStarkAccount = async () => {
-    if (!this.provider) {
-      this.uiConsole('provider not initialized yet');
-      return;
-    }
-    const rpc = new RPC(this.provider as SafeEventEmitterProvider);
-    const starkaccounts = await rpc.getStarkAccount();
-    this.uiConsole(starkaccounts);
-  };
+	getUserInfo = async () => {
+		if (!this.web3auth) {
+			this.uiConsole('web3auth not initialized yet');
+			return;
+		}
+		const user = await this.web3auth.getUserInfo();
+		this.uiConsole(user);
+	};
 
-  getStarkKey = async () => {
-    if (!this.provider) {
-      this.uiConsole('provider not initialized yet');
-      return;
-    }
-    const rpc = new RPC(this.provider as SafeEventEmitterProvider);
-    const starkKey = await rpc.getStarkKey();
-    this.uiConsole(starkKey);
-  };
+	onGetStarkAccount = async () => {
+		if (!this.provider) {
+			this.uiConsole('provider not initialized yet');
+			return;
+		}
+		const rpc = new RPC(this.provider as SafeEventEmitterProvider);
+		const starkaccounts = await rpc.getStarkAccount();
+		this.uiConsole(starkaccounts);
+	};
 
-  onDeployAccount = async () => {
-    if (!this.provider) {
-      this.uiConsole('provider not initialized yet');
-      return;
-    }
-    const rpc = new RPC(this.provider as SafeEventEmitterProvider);
-    const deployaccount = await rpc.deployAccount();
-    this.uiConsole(deployaccount);
-  };
+	getStarkKey = async () => {
+		if (!this.provider) {
+			this.uiConsole('provider not initialized yet');
+			return;
+		}
+		const rpc = new RPC(this.provider as SafeEventEmitterProvider);
+		const starkKey = await rpc.getStarkKey();
+		this.uiConsole(starkKey);
+	};
 
-  logout = async () => {
-    if (!this.web3auth) {
-      this.uiConsole('web3auth not initialized yet');
-      return;
-    }
-    await this.web3auth.logout();
-    this.provider = null;
-    this.uiConsole('logged out');
-  };
+	onDeployAccount = async () => {
+		if (!this.provider) {
+			this.uiConsole('provider not initialized yet');
+			return;
+		}
+		const rpc = new RPC(this.provider as SafeEventEmitterProvider);
+		const deployaccount = await rpc.deployAccount();
+		this.uiConsole(deployaccount);
+	};
 
-  uiConsole(...args: any[]) {
-		const el = document.querySelector("#console-ui>p")
+	logout = async () => {
+		if (!this.web3auth) {
+			this.uiConsole('web3auth not initialized yet');
+			return;
+		}
+		await this.web3auth.logout();
+		this.provider = null;
+		this.uiConsole('logged out');
+	};
+
+	uiConsole(...args: any[]) {
+		const el = document.querySelector('#console-ui>p');
 		if (el) {
-			el.innerHTML = JSON.stringify(args || {}, null, 2)
+			el.innerHTML = JSON.stringify(args || {}, null, 2);
 		}
 	}
 }

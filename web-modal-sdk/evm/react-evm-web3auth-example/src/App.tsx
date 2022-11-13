71a4e896ff212d45c1f1cd30c7b378324edd074a
Saviour1001 <harshghodkar@gmail.com>
2022-11-13 19:33:14 +0530

    added plugins and adapter

--- web-modal-sdk/evm/react-evm-web3auth-example/src/App.tsx
+++ web-modal-sdk/evm/react-evm-web3auth-example/src/App.tsx
@@ -1,257 +1,287 @@
-import { useEffect, useState } from 'react';
-import { Web3Auth } from '@web3auth/modal';
-import { CHAIN_NAMESPACES, SafeEventEmitterProvider } from '@web3auth/base';
-import { OpenloginAdapter } from '@web3auth/openlogin-adapter';
-import './App.css';
-import RPC from './web3RPC'; // for using web3.js
+import { useEffect, useState } from "react";
+import { Web3Auth } from "@web3auth/modal";
+import { CHAIN_NAMESPACES, SafeEventEmitterProvider } from "@web3auth/base";
+import { OpenloginAdapter } from "@web3auth/openlogin-adapter";
+import "./App.css";
+import RPC from "./web3RPC"; // for using web3.js
 //import RPC from "./ethersRPC"; // for using ethers.js
 
+// Plugins
+import { TorusWalletConnectorPlugin } from "@web3auth/torus-wallet-connector-plugin";
+
+// Adapters
+
+import { CoinbaseAdapter } from "@web3auth/coinbase-adapter";
+
 const clientId =
-	'BHr_dKcxC0ecKn_2dZQmQeNdjPgWykMkcodEHkVvPMo71qzOV6SgtoN8KCvFdLN7bf34JOm89vWQMLFmSfIo84A'; // get from https://dashboard.web3auth.io
+  "BHr_dKcxC0ecKn_2dZQmQeNdjPgWykMkcodEHkVvPMo71qzOV6SgtoN8KCvFdLN7bf34JOm89vWQMLFmSfIo84A"; // get from https://dashboard.web3auth.io
 
 function App() {
-	const [web3auth, setWeb3auth] = useState<Web3Auth | null>(null);
-	const [provider, setProvider] = useState<SafeEventEmitterProvider | null>(
-		null,
-	);
+  const [web3auth, setWeb3auth] = useState<Web3Auth | null>(null);
+  const [provider, setProvider] = useState<SafeEventEmitterProvider | null>(
+    null
+  );
 
-	useEffect(() => {
-		const init = async () => {
-			try {
-				const web3auth = new Web3Auth({
-					clientId,
-					chainConfig: {
-						chainNamespace: CHAIN_NAMESPACES.EIP155,
-						chainId: '0x1',
-						rpcTarget: 'https://rpc.ankr.com/eth', // This is the public RPC we have added, please pass on your own endpoint while creating an app
-					},
-					uiConfig: {
-						theme: 'dark',
-						loginMethodsOrder: ['facebook', 'google'],
-						defaultLanguage: 'en',
-						appLogo: 'https://web3auth.io/images/w3a-L-Favicon-1.svg', // Your App Logo Here
-					},
-				});
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const web3auth = new Web3Auth({
+          clientId,
+          chainConfig: {
+            chainNamespace: CHAIN_NAMESPACES.EIP155,
+            chainId: "0x1",
+            rpcTarget: "https://rpc.ankr.com/eth", // This is the public RPC we have added, please pass on your own endpoint while creating an app
+          },
+          uiConfig: {
+            theme: "dark",
+            loginMethodsOrder: ["facebook", "google"],
+            defaultLanguage: "en",
+            appLogo: "https://web3auth.io/images/w3a-L-Favicon-1.svg", // Your App Logo Here
+          },
+        });
 
-				const openloginAdapter = new OpenloginAdapter({
-					loginSettings: {
-						mfaLevel: 'optional',
-					},
-					adapterSettings: {
-						clientId,
-						network: 'testnet',
-						uxMode: 'popup',
-						whiteLabel: {
-							name: 'Your app Name',
-							logoLight: 'https://web3auth.io/images/w3a-L-Favicon-1.svg',
-							logoDark: 'https://web3auth.io/images/w3a-D-Favicon-1.svg',
-							defaultLanguage: 'en',
-							dark: true, // whether to enable dark mode. defaultValue: false
-						},
-					},
-				});
-				web3auth.configureAdapter(openloginAdapter);
+        const openloginAdapter = new OpenloginAdapter({
+          loginSettings: {
+            mfaLevel: "optional",
+          },
+          adapterSettings: {
+            clientId,
+            network: "testnet",
+            uxMode: "popup",
+            whiteLabel: {
+              name: "Your app Name",
+              logoLight: "https://web3auth.io/images/w3a-L-Favicon-1.svg",
+              logoDark: "https://web3auth.io/images/w3a-D-Favicon-1.svg",
+              defaultLanguage: "en",
+              dark: true, // whether to enable dark mode. defaultValue: false
+            },
+          },
+        });
+        web3auth.configureAdapter(openloginAdapter);
 
-				setWeb3auth(web3auth);
+        // adding torus wallet connector plugin
 
-				await web3auth.initModal();
-				if (web3auth.provider) {
-					setProvider(web3auth.provider);
-				}
-			} catch (error) {
-				console.error(error);
-			}
-		};
+        const torusPlugin = new TorusWalletConnectorPlugin({
+          torusWalletOpts: {},
+          walletInitOptions: {
+            whiteLabel: {
+              theme: { isDark: true, colors: { primary: "#00a8ff" } },
+              logoDark: "https://web3auth.io/images/w3a-L-Favicon-1.svg",
+              logoLight: "https://web3auth.io/images/w3a-D-Favicon-1.svg",
+            },
+            useWalletConnect: true,
+            enableLogging: true,
+          },
+        });
+        await web3auth.addPlugin(torusPlugin);
 
-		init();
-	}, []);
+        // adding coinbase adapter
 
-	const login = async () => {
-		if (!web3auth) {
-			uiConsole('web3auth not initialized yet');
-			return;
-		}
-		const web3authProvider = await web3auth.connect();
-		setProvider(web3authProvider);
-		uiConsole('Logged in Successfully!');
-	};
+        const coinbaseAdapter = new CoinbaseAdapter({
+          clientId,
+        });
+        web3auth.configureAdapter(coinbaseAdapter);
 
-	const authenticateUser = async () => {
-		if (!web3auth) {
-			uiConsole('web3auth not initialized yet');
-			return;
-		}
-		const idToken = await web3auth.authenticateUser();
-		uiConsole(idToken);
-	};
+        setWeb3auth(web3auth);
 
-	const getUserInfo = async () => {
-		if (!web3auth) {
-			uiConsole('web3auth not initialized yet');
-			return;
-		}
-		const user = await web3auth.getUserInfo();
-		uiConsole(user);
-	};
+        await web3auth.initModal();
+        if (web3auth.provider) {
+          setProvider(web3auth.provider);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-	const logout = async () => {
-		if (!web3auth) {
-			uiConsole('web3auth not initialized yet');
-			return;
-		}
-		await web3auth.logout();
-		setProvider(null);
-	};
+    init();
+  }, []);
 
-	const getChainId = async () => {
-		if (!provider) {
-			uiConsole('provider not initialized yet');
-			return;
-		}
-		const rpc = new RPC(provider);
-		const chainId = await rpc.getChainId();
-		uiConsole(chainId);
-	};
-	const getAccounts = async () => {
-		if (!provider) {
-			uiConsole('provider not initialized yet');
-			return;
-		}
-		const rpc = new RPC(provider);
-		const address = await rpc.getAccounts();
-		uiConsole(address);
-	};
+  const login = async () => {
+    if (!web3auth) {
+      uiConsole("web3auth not initialized yet");
+      return;
+    }
+    const web3authProvider = await web3auth.connect();
+    setProvider(web3authProvider);
+    uiConsole("Logged in Successfully!");
+  };
 
-	const getBalance = async () => {
-		if (!provider) {
-			uiConsole('provider not initialized yet');
-			return;
-		}
-		const rpc = new RPC(provider);
-		const balance = await rpc.getBalance();
-		uiConsole(balance);
-	};
+  const authenticateUser = async () => {
+    if (!web3auth) {
+      uiConsole("web3auth not initialized yet");
+      return;
+    }
+    const idToken = await web3auth.authenticateUser();
+    uiConsole(idToken);
+  };
 
-	const sendTransaction = async () => {
-		if (!provider) {
-			uiConsole('provider not initialized yet');
-			return;
-		}
-		const rpc = new RPC(provider);
-		const receipt = await rpc.sendTransaction();
-		uiConsole(receipt);
-	};
+  const getUserInfo = async () => {
+    if (!web3auth) {
+      uiConsole("web3auth not initialized yet");
+      return;
+    }
+    const user = await web3auth.getUserInfo();
+    uiConsole(user);
+  };
 
-	const signMessage = async () => {
-		if (!provider) {
-			uiConsole('provider not initialized yet');
-			return;
-		}
-		const rpc = new RPC(provider);
-		const signedMessage = await rpc.signMessage();
-		uiConsole(signedMessage);
-	};
+  const logout = async () => {
+    if (!web3auth) {
+      uiConsole("web3auth not initialized yet");
+      return;
+    }
+    await web3auth.logout();
+    setProvider(null);
+  };
 
-	const getPrivateKey = async () => {
-		if (!provider) {
-			uiConsole('provider not initialized yet');
-			return;
-		}
-		const rpc = new RPC(provider);
-		const privateKey = await rpc.getPrivateKey();
-		uiConsole(privateKey);
-	};
+  const getChainId = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const chainId = await rpc.getChainId();
+    uiConsole(chainId);
+  };
+  const getAccounts = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const address = await rpc.getAccounts();
+    uiConsole(address);
+  };
 
-	function uiConsole(...args: any[]): void {
-		const el = document.querySelector('#console>p');
-		if (el) {
-			el.innerHTML = JSON.stringify(args || {}, null, 2);
-		}
-	}
+  const getBalance = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const balance = await rpc.getBalance();
+    uiConsole(balance);
+  };
 
-	const loggedInView = (
-		<>
-			<div className='flex-container'>
-				<div>
-					<button onClick={getUserInfo} className='card'>
-						Get User Info
-					</button>
-				</div>
-				<div>
-					<button onClick={authenticateUser} className='card'>
-						Get ID Token
-					</button>
-				</div>
-				<div>
-					<button onClick={getChainId} className='card'>
-						Get Chain ID
-					</button>
-				</div>
-				<div>
-					<button onClick={getAccounts} className='card'>
-						Get Accounts
-					</button>
-				</div>
-				<div>
-					<button onClick={getBalance} className='card'>
-						Get Balance
-					</button>
-				</div>
-				<div>
-					<button onClick={signMessage} className='card'>
-						Sign Message
-					</button>
-				</div>
-				<div>
-					<button onClick={sendTransaction} className='card'>
-						Send Transaction
-					</button>
-				</div>
-				<div>
-					<button onClick={getPrivateKey} className='card'>
-						Get Private Key
-					</button>
-				</div>
-				<div>
-					<button onClick={logout} className='card'>
-						Log Out
-					</button>
-				</div>
-			</div>
-			<div id='console' style={{ whiteSpace: 'pre-line' }}>
-				<p style={{ whiteSpace: 'pre-line' }}></p>
-			</div>
-		</>
-	);
+  const sendTransaction = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const receipt = await rpc.sendTransaction();
+    uiConsole(receipt);
+  };
 
-	const unloggedInView = (
-		<button onClick={login} className='card'>
-			Login
-		</button>
-	);
+  const signMessage = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const signedMessage = await rpc.signMessage();
+    uiConsole(signedMessage);
+  };
 
-	return (
-		<div className='container'>
-			<h1 className='title'>
-				<a target='_blank' href='http://web3auth.io/' rel='noreferrer'>
-					Web3Auth{' '}
-				</a>
-				& ReactJS Ethereum Example
-			</h1>
+  const getPrivateKey = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const privateKey = await rpc.getPrivateKey();
+    uiConsole(privateKey);
+  };
 
-			<div className='grid'>{provider ? loggedInView : unloggedInView}</div>
+  function uiConsole(...args: any[]): void {
+    const el = document.querySelector("#console>p");
+    if (el) {
+      el.innerHTML = JSON.stringify(args || {}, null, 2);
+    }
+  }
 
-			<footer className='footer'>
-				<a
-					href='https://github.com/Web3Auth/examples/tree/main/web-modal-sdk/evm/react-evm-web3auth-example'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Source code
-				</a>
-			</footer>
-		</div>
-	);
+  const loggedInView = (
+    <>
+      <div className="flex-container">
+        <div>
+          <button onClick={getUserInfo} className="card">
+            Get User Info
+          </button>
+        </div>
+        <div>
+          <button onClick={authenticateUser} className="card">
+            Get ID Token
+          </button>
+        </div>
+        <div>
+          <button onClick={getChainId} className="card">
+            Get Chain ID
+          </button>
+        </div>
+        <div>
+          <button onClick={getAccounts} className="card">
+            Get Accounts
+          </button>
+        </div>
+        <div>
+          <button onClick={getBalance} className="card">
+            Get Balance
+          </button>
+        </div>
+        <div>
+          <button onClick={signMessage} className="card">
+            Sign Message
+          </button>
+        </div>
+        <div>
+          <button onClick={sendTransaction} className="card">
+            Send Transaction
+          </button>
+        </div>
+        <div>
+          <button onClick={getPrivateKey} className="card">
+            Get Private Key
+          </button>
+        </div>
+        <div>
+          <button onClick={logout} className="card">
+            Log Out
+          </button>
+        </div>
+      </div>
+      <div id="console" style={{ whiteSpace: "pre-line" }}>
+        <p style={{ whiteSpace: "pre-line" }}></p>
+      </div>
+    </>
+  );
+
+  const unloggedInView = (
+    <button onClick={login} className="card">
+      Login
+    </button>
+  );
+
+  return (
+    <div className="container">
+      <h1 className="title">
+        <a target="_blank" href="http://web3auth.io/" rel="noreferrer">
+          Web3Auth{" "}
+        </a>
+        & ReactJS Ethereum Example
+      </h1>
+
+      <div className="grid">{provider ? loggedInView : unloggedInView}</div>
+
+      <footer className="footer">
+        <a
+          href="https://github.com/Web3Auth/examples/tree/main/web-modal-sdk/evm/react-evm-web3auth-example"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Source code
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;

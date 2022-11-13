12ecccd2f90c0c38ddd7e310425a21305bb8ddd8
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2022-11-14 03:26:17 +0530

    rn expo restructure

--- react-native/rn-expo-example/App.js
+++ react-native/rn-expo-example/App.js
@@ -4,10 +4,7 @@ import Web3Auth, { LOGIN_PROVIDER, OPENLOGIN_NETWORK } from "@web3auth/react-nat
 import Constants, { AppOwnership } from "expo-constants";
 import * as Linking from "expo-linking";
 import * as WebBrowser from "expo-web-browser";
-import { Buffer } from "buffer";
-import "@ethersproject/shims";
-import { ethers } from "ethers";
-global.Buffer = global.Buffer || Buffer;
+import RPC from './ethersRPC'; // for using ethers.js
     
 const resolvedRedirectUrl =
   Constants.appOwnership == AppOwnership.Expo || Constants.appOwnership == AppOwnership.Guest
@@ -45,75 +42,30 @@ export default function App() {
   };
 
   const getChainId = async () => {
-    try {
-      setConsole("Getting chain id");
-      const ethersProvider = ethers.getDefaultProvider(providerUrl);
-      const networkDetails = await ethersProvider.getNetwork();
-      uiConsole(networkDetails);
-    } catch (e) {
-      uiConsole(e);
-    }
-  }
-  
+    setConsole('Getting chain id');
+    const networkDetails = await RPC.getChainId();
+    uiConsole(networkDetails);
+  };
+
   const getAccounts = async () => {
-    try {
-      setConsole("Getting account");
-      const wallet = new ethers.Wallet(key)
-      const address = await wallet.address;
-      uiConsole(address);
-    } catch (e) {
-      uiConsole(e);
-    }
+    setConsole('Getting account');
+    const address = await RPC.getAccounts(key);
+    uiConsole(address);
   };
   const getBalance = async () => {
-    try {
-      setConsole("Fetching balance");
-      const ethersProvider = ethers.getDefaultProvider(providerUrl);
-      const wallet = new ethers.Wallet(key, ethersProvider);
-      const balance = await wallet.getBalance();
-      uiConsole(balance);
-    } catch (e) {
-      uiConsole(e);
-    }
+    setConsole('Fetching balance');
+    const balance = await RPC.getBalance(key);
+    uiConsole(balance);
   };
   const sendTransaction = async () => {
-    try {
-      setConsole("Sending transaction");
-      const ethersProvider = ethers.getDefaultProvider(providerUrl);
-      const wallet = new ethers.Wallet(key, ethersProvider);
-
-      const destination = "0x40e1c367Eca34250cAF1bc8330E9EddfD403fC56";
-
-      // Convert 1 ether to wei
-      const amount = ethers.utils.parseEther("0.001");
-
-      // Submit transaction to the blockchain
-      const tx = await wallet.sendTransaction({
-        to: destination,
-        value: amount,
-        maxPriorityFeePerGas: "5000000000", // Max priority fee per gas
-        maxFeePerGas: "6000000000000", // Max fee per gas
-      });
-      uiConsole(tx);
-    } catch (e) {
-      uiConsole(e);
-    }
+    setConsole('Sending transaction');
+    const tx = await RPC.sendTransaction(key);
+    uiConsole(tx);
   };
   const signMessage = async () => {
-    try {
-      setConsole("Signing message");
-      const ethersProvider = ethers.getDefaultProvider(providerUrl);
-      const wallet = new ethers.Wallet(key, ethersProvider);
-
-      const originalMessage = "YOUR_MESSAGE";
-
-      // Sign the message
-      const signedMessage = await wallet.signMessage(originalMessage);
-
-      uiConsole(signedMessage);
-    } catch (e) {
-      uiConsole(e);
-    }
+    setConsole('Signing message');
+    const message = await RPC.signMessage(key);
+    uiConsole(message);
   };
 
   const uiConsole = (...args) => {
@@ -121,21 +73,21 @@ export default function App() {
   };
 
   const loggedInView = (
-    <View style={{ flex: 2, alignItems: "center", justifyContent: "center" }}>
-      <Button title="Get User Info" onPress={() => uiConsole(userInfo)} style={styles.button} />
-      <Button title="Get Chain ID" onPress={() => getChainId()} style={styles.button} />
-      <Button title="Get Accounts" onPress={() => getAccounts()} style={styles.button} />
-      <Button title="Get Balance" onPress={() => getBalance()} style={styles.button} />
-      <Button title="Send Transaction" onPress={() => sendTransaction()} style={styles.button} />
-      <Button title="Sign Message" onPress={() => signMessage()} style={styles.button} />
-      <Button title="Get Private Key" onPress={() => uiConsole(key)} style={styles.button} />
-      <Button title="Log Out" onPress={() => setKey("")} style={styles.button} />
+    <View style={styles.buttonArea}>
+      <Button title="Get User Info" onPress={() => uiConsole(userInfo)} />
+      <Button title="Get Chain ID" onPress={() => getChainId()} />
+      <Button title="Get Accounts" onPress={() => getAccounts()} />
+      <Button title="Get Balance" onPress={() => getBalance()} />
+      <Button title="Send Transaction" onPress={() => sendTransaction()} />
+      <Button title="Sign Message" onPress={() => signMessage()} />
+      <Button title="Get Private Key" onPress={() => uiConsole(key)} />
+      <Button title="Log Out" onPress={() => setKey('')} />
     </View>
   );
-  
+
   const unloggedInView = (
-    <View style={{ flex: 2, alignItems: "center", justifyContent: "center" }}>
-      <Button title="Login with Web3Auth" onPress={login} style={styles.button} />
+    <View style={styles.buttonArea}>
+      <Button title="Login with Web3Auth" onPress={login} />
     </View>
   );
 
@@ -143,7 +95,7 @@ export default function App() {
     <View style={styles.container}>
       {key ? loggedInView : unloggedInView}
       <View style={styles.consoleArea}>
-        <Text style={{ padding: 10 }}>Console:</Text>
+        <Text style={styles.consoleText}>Console:</Text>
         <ScrollView style={styles.console}>
           <Text>{console}</Text>
         </ScrollView>
@@ -155,25 +107,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  button: {
-    margin: 5,
-    padding: 5
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 50,
+    paddingBottom: 30,
   },
   consoleArea: {
-    margin: 20, 
-    alignItems: "center", 
-    justifyContent: "center", 
+    margin: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
     flex: 1,
   },
   console: {
     flex: 1,
-    backgroundColor: "#CCCCCC",
-    color: "#ffffff",
+    backgroundColor: '#CCCCCC',
+    color: '#ffffff',
     padding: 10,
     width: Dimensions.get('window').width - 60,
-  }
+  },
+  consoleText: {
+    padding: 10,
+  },
+  buttonArea: {
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    paddingBottom: 30,
+  },
 });

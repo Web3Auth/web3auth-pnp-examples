e0bfe49cac8545aaca4e981e37039a3c62d9d53f
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2022-12-19 18:31:25 +0530

    Update self host app to be one pager

--- self-host/self-host-react-example/src/App.tsx
+++ self-host/self-host-react-example/src/App.tsx
@@ -1,27 +1,51 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import SecurityQuestionsModule from '@tkey/security-questions';
 import swal from 'sweetalert';
-import { tKey } from './lib/web3auth';
+import ThresholdKey from '@tkey/default';
+import WebStorageModule from '@tkey/web-storage';
+import SecurityQuestionsModule from '@tkey/security-questions';
 
 function App() {
-	const [user, setUser] = useState(null);
+	const [user, setUser] = useState<any>(null);
 	const [privateKey, setPrivateKey] = useState<any>();
+	const [tKey, setTKey] = useState<ThresholdKey | null>(null);
 
 	// Init Service Provider inside the useEffect Method
 	useEffect(() => {
 		const init = async () => {
+			// Configuration of Service Provider
+			const customAuthArgs = {
+				baseUrl: `${window.location.origin}/serviceworker`,
+				network: 'cyan',
+			};
+			// Configuration of Modules
+			const webStorageModule = new WebStorageModule(); // For 2/2
+			const securityQuestionsModule = new SecurityQuestionsModule(); // For 2/3
+
+			// Instantiation of tKey
+			const tKey = new ThresholdKey({
+				modules: {
+					webStorage: webStorageModule,
+					securityQuestions: securityQuestionsModule,
+				},
+				customAuthArgs: customAuthArgs as any,
+			});
+			setTKey(tKey);
 			// Initialization of Service Provider
 			await (tKey.serviceProvider as any).init();
 			try {
 			} catch (error) {
-				console.error(error);
+				uiConsole(error);
 			}
 		};
 		init();
 	}, []);
 
 	const triggerLogin = async () => {
+		if (!tKey) {
+			uiConsole("tKey not initialized yet");
+			return;
+		}
 		try {
 			// Triggering Login using Service Provider ==> opens the popup
 			const loginResponse = await (tKey.serviceProvider as any).triggerLogin({
@@ -31,23 +55,27 @@ function App() {
 					'774338308167-q463s7kpvja16l4l0kko3nb925ikds2p.apps.googleusercontent.com',
 			});
 			setUser(loginResponse.userInfo);
-			console.log('Public Key : ' + loginResponse.publicAddress);
-			console.log('Email : ' + loginResponse.userInfo.email);
+			uiConsole('Public Key : ' + loginResponse.publicAddress);
+			uiConsole('Email : ' + loginResponse.userInfo.email);
 		} catch (error) {
-			console.log(error);
+			uiConsole(error);
 		}
 	};
 
 	const initializeNewKey = async () => {
+		if (!tKey) {
+			uiConsole("tKey not initialized yet");
+			return;
+		}
 		try {
 			await triggerLogin(); // Calls the triggerLogin() function above
 			// Initialization of tKey
 			await tKey.initialize(); // 1/2 flow
 			// Gets the deviceShare
 			try {
-				await (tKey.modules.webStorage as any).inputShareFromWebStorage(); // 2/2 flow
-			} catch (error) {
-				console.error(error);
+				await (tKey.modules.webStorage as WebStorageModule).inputShareFromWebStorage(); // 2/2 flow
+			} catch (e) {
+				uiConsole(e);
 				await recoverShare();
 			}
 
@@ -62,30 +90,38 @@ function App() {
 				);
 			}
 		} catch (error) {
-			console.error(error, 'caught');
+			uiConsole(error, 'caught');
 		}
 	};
 
 	const changeSecurityQuestionAndAnswer = async () => {
+		if (!tKey) {
+			uiConsole("tKey not initialized yet");
+			return;
+		}
 		// swal is just a pretty dialog box
-			swal('Enter password (>10 characters)', {
-				content: 'input' as any,
-			}).then(async value => {
-				if (value.length > 10) {
-					await (
-						tKey.modules.securityQuestions as SecurityQuestionsModule
-					).changeSecurityQuestionAndAnswer(value, 'whats your password?');
-					swal('Success', 'Successfully changed new share with password.', 'success');
-					console.log('Successfully changed new share with password.');
-				} else {
-					swal('Error', 'Password must be >= 11 characters', 'error');
-				}
-			});
+		swal('Enter password (>10 characters)', {
+			content: 'input' as any,
+		}).then(async value => {
+			if (value.length > 10) {
+				await (
+					tKey.modules.securityQuestions as SecurityQuestionsModule
+				).changeSecurityQuestionAndAnswer(value, 'whats your password?');
+				swal('Success', 'Successfully changed new share with password.', 'success');
+				uiConsole('Successfully changed new share with password.');
+			} else {
+				swal('Error', 'Password must be >= 11 characters', 'error');
+			}
+		});
 		const keyDetails = await tKey.getKeyDetails();
 		uiConsole(keyDetails);
 	};
 
 	const generateNewShareWithPassword = async () => {
+		if (!tKey) {
+			uiConsole("tKey not initialized yet");
+			return;
+		}
 		// swal is just a pretty dialog box
 		swal('Enter password (>10 characters)', {
 			content: 'input' as any,
@@ -99,7 +135,7 @@ function App() {
 						'whats your password?',
 					);
 					swal('Success', 'Successfully generated new share with password.', 'success');
-					console.log('Successfully generated new share with password.');
+					uiConsole('Successfully generated new share with password.');
 				} catch (error) {
 					swal('Error', (error as any)?.message.toString(), 'error');
 				}
@@ -110,6 +146,10 @@ function App() {
 	}
 
 	const recoverShare = async () => {
+		if (!tKey) {
+			uiConsole("tKey not initialized yet");
+			return;
+		}
 		// swal is just a pretty dialog box
 		swal('Enter password (>10 characters)', {
 			content: 'input' as any,
@@ -117,14 +157,21 @@ function App() {
 			if (value.length > 10) {
 				try {
 					await (tKey.modules.securityQuestions as SecurityQuestionsModule).inputShareFromSecurityQuestions(value); // 2/2 flow
-					tKey.reconstructKey();
+					const { requiredShares } = tKey.getKeyDetails();
+					if (requiredShares <= 0) {
+						const reconstructedKey = await tKey.reconstructKey();
+						setPrivateKey(reconstructedKey?.privKey.toString("hex"))
+						uiConsole(
+							'Private Key: ' + reconstructedKey.privKey.toString("hex"),
+						);
+					}
 					const shareStore = await tKey.generateNewShare();
-					await (tKey.modules.webStorage as any).storeDeviceShare(shareStore.newShareStores[1]);
+					await (tKey.modules.webStorage as WebStorageModule).storeDeviceShare(shareStore.newShareStores[1]);
 					swal('Success', 'Successfully logged you in with the recovery password.', 'success');
-					console.log('Successfully logged you in with the recovery password.');
+					uiConsole('Successfully logged you in with the recovery password.');
 				} catch (error) {
 					swal('Error', (error as any)?.message.toString(), 'error');
-					console.log(error);
+					uiConsole(error);
 					logout();
 				}
 			} else {
@@ -135,12 +182,16 @@ function App() {
 	}
 
 	const keyDetails = async () => {
+		if (!tKey) {
+			uiConsole("tKey not initialized yet");
+			return;
+		}
 		const keyDetails = await tKey.getKeyDetails();
 		uiConsole(keyDetails);
 	};
 
 	const logout = (): void => {
-		console.log('Log out');
+		uiConsole('Log out');
 		setUser(null);
 	};
 

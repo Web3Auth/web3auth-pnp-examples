560cbed5c38ad58d4749c862ab914e0fca224dad
Saviour1001 <harshghodkar@gmail.com>
2022-12-21 19:49:46 +0800

    updated more examples

--- web-modal-sdk/starkex/angular-starkex-web3auth-example/src/app/app.component.ts
+++ web-modal-sdk/starkex/angular-starkex-web3auth-example/src/app/app.component.ts
@@ -1,141 +1,134 @@
 import { Component } from '@angular/core';
 import { Web3Auth } from '@web3auth/modal';
 import {
-	WALLET_ADAPTERS,
-	CHAIN_NAMESPACES,
-	SafeEventEmitterProvider,
+  WALLET_ADAPTERS,
+  CHAIN_NAMESPACES,
+  SafeEventEmitterProvider,
 } from '@web3auth/base';
-import { OpenloginAdapter } from '@web3auth/openlogin-adapter';
 import RPC from './starkexRPC';
 const clientId =
-	'BEglQSgt4cUWcj6SKRdu5QkOXTsePmMcusG5EAoyjyOYKlVRjIF1iCNnMOTfpzCiunHRrMui8TIwQPXdkQ8Yxuk'; // get from https://dashboard.web3auth.io
+  'BEglQSgt4cUWcj6SKRdu5QkOXTsePmMcusG5EAoyjyOYKlVRjIF1iCNnMOTfpzCiunHRrMui8TIwQPXdkQ8Yxuk'; // get from https://dashboard.web3auth.io
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css'],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-	title = 'angular-app';
-	web3auth: Web3Auth | null = null;
-	provider: SafeEventEmitterProvider | null = null;
-	isModalLoaded = false;
+  title = 'angular-app';
+  web3auth: Web3Auth | null = null;
+  provider: SafeEventEmitterProvider | null = null;
+  isModalLoaded = false;
 
-	async ngOnInit() {
-		this.web3auth = new Web3Auth({
-			clientId,
-			chainConfig: {
-				chainNamespace: CHAIN_NAMESPACES.OTHER,
-			},
-		});
-		const web3auth = this.web3auth;
+  async ngOnInit() {
+    this.web3auth = new Web3Auth({
+      clientId,
+      chainConfig: {
+        chainNamespace: CHAIN_NAMESPACES.OTHER,
+      },
+      web3AuthNetwork: 'cyan',
+    });
+    const web3auth = this.web3auth;
 
-		const openloginAdapter = new OpenloginAdapter({
-			adapterSettings: {
-				network: 'cyan',
-			},
-		});
-		web3auth.configureAdapter(openloginAdapter);
+    await web3auth.initModal();
+    if (web3auth.provider) {
+      this.provider = web3auth.provider;
+    }
+    this.isModalLoaded = true;
+  }
 
-		await web3auth.initModal();
-		if (web3auth.provider) {
-			this.provider = web3auth.provider;
-		}
-		this.isModalLoaded = true;
-	}
+  login = async () => {
+    if (!this.web3auth) {
+      this.uiConsole('web3auth not initialized yet');
+      return;
+    }
+    const web3auth = this.web3auth;
+    this.provider = await web3auth.connect();
+    this.uiConsole('Logged in Successfully!');
+  };
 
-	login = async () => {
-		if (!this.web3auth) {
-			this.uiConsole('web3auth not initialized yet');
-			return;
-		}
-		const web3auth = this.web3auth;
-		this.provider = await web3auth.connect();
-		this.uiConsole('Logged in Successfully!');
-	};
+  authenticateUser = async () => {
+    if (!this.web3auth) {
+      this.uiConsole('web3auth not initialized yet');
+      return;
+    }
+    const id_token = await this.web3auth.authenticateUser();
+    this.uiConsole(id_token);
+  };
 
-	authenticateUser = async () => {
-		if (!this.web3auth) {
-			this.uiConsole('web3auth not initialized yet');
-			return;
-		}
-		const id_token = await this.web3auth.authenticateUser();
-		this.uiConsole(id_token);
-	};
+  getUserInfo = async () => {
+    if (!this.web3auth) {
+      this.uiConsole('web3auth not initialized yet');
+      return;
+    }
+    const user = await this.web3auth.getUserInfo();
+    this.uiConsole(user);
+  };
 
-	getUserInfo = async () => {
-		if (!this.web3auth) {
-			this.uiConsole('web3auth not initialized yet');
-			return;
-		}
-		const user = await this.web3auth.getUserInfo();
-		this.uiConsole(user);
-	};
+  onGetStarkAccount = async () => {
+    if (!this.provider) {
+      this.uiConsole('provider not initialized yet');
+      return;
+    }
+    const rpc = new RPC(this.provider as SafeEventEmitterProvider);
+    const starkaccounts = await rpc.getStarkAccount();
+    this.uiConsole(starkaccounts);
+  };
 
-	onGetStarkAccount = async () => {
-		if (!this.provider) {
-			this.uiConsole('provider not initialized yet');
-			return;
-		}
-		const rpc = new RPC(this.provider as SafeEventEmitterProvider);
-		const starkaccounts = await rpc.getStarkAccount();
-		this.uiConsole(starkaccounts);
-	};
+  getStarkKey = async () => {
+    if (!this.provider) {
+      this.uiConsole('provider not initialized yet');
+      return;
+    }
+    const rpc = new RPC(this.provider as SafeEventEmitterProvider);
+    const starkKey = await rpc.getStarkKey();
+    this.uiConsole(starkKey);
+  };
 
-	getStarkKey = async () => {
-		if (!this.provider) {
-			this.uiConsole('provider not initialized yet');
-			return;
-		}
-		const rpc = new RPC(this.provider as SafeEventEmitterProvider);
-		const starkKey = await rpc.getStarkKey();
-		this.uiConsole(starkKey);
-	};
+  onMintRequest = async () => {
+    if (!this.provider) {
+      this.uiConsole('provider not initialized yet');
+      return;
+    }
+    const rpc = new RPC(this.provider as SafeEventEmitterProvider);
+    const request = await rpc.onMintRequest();
+    this.uiConsole(request);
+  };
 
-	onMintRequest = async () => {
-		if (!this.provider) {
-			this.uiConsole('provider not initialized yet');
-			return;
-		}
-		const rpc = new RPC(this.provider as SafeEventEmitterProvider);
-		const request = await rpc.onMintRequest();
-		this.uiConsole(request);
-	};
+  onDepositRequest = async () => {
+    if (!this.provider) {
+      this.uiConsole('provider not initialized yet');
+      return;
+    }
+    const rpc = new RPC(this.provider as SafeEventEmitterProvider);
+    const request = await rpc.onDepositRequest();
+    this.uiConsole(request);
+  };
 
-	onDepositRequest = async () => {
-		if (!this.provider) {
-			this.uiConsole('provider not initialized yet');
-			return;
-		}
-		const rpc = new RPC(this.provider as SafeEventEmitterProvider);
-		const request = await rpc.onDepositRequest();
-		this.uiConsole(request);
-	};
+  onWithdrawalRequest = async () => {
+    if (!this.provider) {
+      this.uiConsole('provider not initialized yet');
+      return;
+    }
+    const rpc = new RPC(this.provider as SafeEventEmitterProvider);
+    const request = await rpc.onWithdrawalRequest();
+    this.uiConsole(request);
+  };
 
-	onWithdrawalRequest = async () => {
-		if (!this.provider) {
-			this.uiConsole('provider not initialized yet');
-			return;
-		}
-		const rpc = new RPC(this.provider as SafeEventEmitterProvider);
-		const request = await rpc.onWithdrawalRequest();
-		this.uiConsole(request);
-	};
+  logout = async () => {
+    if (!this.web3auth) {
+      this.uiConsole('web3auth not initialized yet');
+      return;
+    }
+    await this.web3auth.logout();
+    this.provider = null;
+    this.uiConsole('logged out');
+  };
 
-	logout = async () => {
-		if (!this.web3auth) {
-			this.uiConsole('web3auth not initialized yet');
-			return;
-		}
-		await this.web3auth.logout();
-		this.provider = null;
-		this.uiConsole('logged out');
-	};
-
-	uiConsole(...args: any[]) {
-		const el = document.querySelector('#console-ui>p');
-		if (el) {
-			el.innerHTML = JSON.stringify(args || {}, null, 2);
-		}
-	}
+  uiConsole(...args: any[]) {
+    const el = document.querySelector('#console-ui>p');
+    if (el) {
+      el.innerHTML = JSON.stringify(args || {}, null, 2);
+    }
+  }
 }
--- web-modal-sdk/starknet/vue-starknet-web3auth-example/src/Home.vue
+++ web-modal-sdk/starknet/vue-starknet-web3auth-example/src/Home.vue
@@ -1,275 +1,268 @@
 <template>
-	<div id="app">
-		<h2>Web3Auth Vue.js StarkNet Example</h2>
+  <div id="app">
+    <h2>Web3Auth Vue.js StarkNet Example</h2>
 
-		<button
-			v-if="!loggedin"
-			class="card"
-			@click="login"
-			style="cursor: pointer"
-		>
-			Login
-		</button>
+    <button
+      v-if="!loggedin"
+      class="card"
+      @click="login"
+      style="cursor: pointer"
+    >
+      Login
+    </button>
 
-		<div v-if="loggedin">
-			<div class="flex-container">
-				<div>
-					<button class="card" @click="getUserInfo" style="cursor: pointer">
-						Get User Info
-					</button>
-				</div>
-				<div>
-					<button
-						class="card"
-						@click="authenticateUser"
-						style="cursor: pointer"
-					>
-						Get ID Token
-					</button>
-				</div>
-				<div>
-					<button
-						class="card"
-						@click="onGetStarkAccount"
-						style="cursor: pointer"
-					>
-						Get Stark Accounts
-					</button>
-				</div>
-				<div>
-					<button class="card" @click="getStarkKey" style="cursor: pointer">
-						Get Stark Key
-					</button>
-				</div>
-				<div>
-					<button class="card" @click="onDeployAccount" style="cursor: pointer">
-						Deploy Account
-					</button>
-				</div>
-				<div>
-					<button class="card" @click="logout" style="cursor: pointer">
-						Logout
-					</button>
-				</div>
-			</div>
-			<div id="console" style="white-space: pre-line">
-				<p style="white-space: pre-line"></p>
-			</div>
-		</div>
-	</div>
+    <div v-if="loggedin">
+      <div class="flex-container">
+        <div>
+          <button class="card" @click="getUserInfo" style="cursor: pointer">
+            Get User Info
+          </button>
+        </div>
+        <div>
+          <button
+            class="card"
+            @click="authenticateUser"
+            style="cursor: pointer"
+          >
+            Get ID Token
+          </button>
+        </div>
+        <div>
+          <button
+            class="card"
+            @click="onGetStarkAccount"
+            style="cursor: pointer"
+          >
+            Get Stark Accounts
+          </button>
+        </div>
+        <div>
+          <button class="card" @click="getStarkKey" style="cursor: pointer">
+            Get Stark Key
+          </button>
+        </div>
+        <div>
+          <button class="card" @click="onDeployAccount" style="cursor: pointer">
+            Deploy Account
+          </button>
+        </div>
+        <div>
+          <button class="card" @click="logout" style="cursor: pointer">
+            Logout
+          </button>
+        </div>
+      </div>
+      <div id="console" style="white-space: pre-line">
+        <p style="white-space: pre-line"></p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { ref, onMounted } from 'vue';
-import { Web3Auth } from '@web3auth/modal';
+import { ref, onMounted } from "vue";
+import { Web3Auth } from "@web3auth/modal";
 import {
-	WALLET_ADAPTERS,
-	CHAIN_NAMESPACES,
-	SafeEventEmitterProvider,
-} from '@web3auth/base';
-import { OpenloginAdapter } from '@web3auth/openlogin-adapter';
-import RPC from './starknetRPC';
+  WALLET_ADAPTERS,
+  CHAIN_NAMESPACES,
+  SafeEventEmitterProvider,
+} from "@web3auth/base";
+import RPC from "./starknetRPC";
 
 export default {
-	name: 'Home',
-	props: {
-		msg: String,
-	},
-	setup() {
-		const loggedin = ref<boolean>(false);
-		const loading = ref<boolean>(false);
-		const loginButtonStatus = ref<string>('');
-		const connecting = ref<boolean>(false);
-		let provider = ref<SafeEventEmitterProvider | any>(null);
-		const clientId =
-			'BEglQSgt4cUWcj6SKRdu5QkOXTsePmMcusG5EAoyjyOYKlVRjIF1iCNnMOTfpzCiunHRrMui8TIwQPXdkQ8Yxuk'; // get from https://dashboard.web3auth.io
+  name: "Home",
+  props: {
+    msg: String,
+  },
+  setup() {
+    const loggedin = ref<boolean>(false);
+    const loading = ref<boolean>(false);
+    const loginButtonStatus = ref<string>("");
+    const connecting = ref<boolean>(false);
+    let provider = ref<SafeEventEmitterProvider | any>(null);
+    const clientId =
+      "BEglQSgt4cUWcj6SKRdu5QkOXTsePmMcusG5EAoyjyOYKlVRjIF1iCNnMOTfpzCiunHRrMui8TIwQPXdkQ8Yxuk"; // get from https://dashboard.web3auth.io
 
-		const web3auth = new Web3Auth({
-			clientId,
-			chainConfig: {
-				chainNamespace: CHAIN_NAMESPACES.OTHER,
-			},
-		});
+    const web3auth = new Web3Auth({
+      clientId,
+      chainConfig: {
+        chainNamespace: CHAIN_NAMESPACES.OTHER,
+      },
+      web3AuthNetwork: "cyan",
+    });
 
-		onMounted(async () => {
-			try {
-				loading.value = true;
-				loggedin.value = false;
+    onMounted(async () => {
+      try {
+        loading.value = true;
+        loggedin.value = false;
 
-				const openloginAdapter = new OpenloginAdapter({
-					adapterSettings: {
-						network: 'cyan',
-					},
-				});
-				web3auth.configureAdapter(openloginAdapter);
+        await web3auth.initModal();
+        if (web3auth.provider) {
+          provider = web3auth.provider;
+          loggedin.value = true;
+        }
+      } catch (error) {
+        uiConsole("error", error);
+      } finally {
+        loading.value = false;
+      }
+    });
 
-				await web3auth.initModal();
-				if (web3auth.provider) {
-					provider = web3auth.provider;
-					loggedin.value = true;
-				}
-			} catch (error) {
-				uiConsole('error', error);
-			} finally {
-				loading.value = false;
-			}
-		});
+    const login = async () => {
+      if (!web3auth) {
+        uiConsole("web3auth not initialized yet");
+        return;
+      }
+      provider = await web3auth.connect();
+      loggedin.value = true;
+      uiConsole("Logged in Successfully!");
+    };
 
-		const login = async () => {
-			if (!web3auth) {
-				uiConsole('web3auth not initialized yet');
-				return;
-			}
-			provider = await web3auth.connect();
-			loggedin.value = true;
-			uiConsole('Logged in Successfully!');
-		};
+    const authenticateUser = async () => {
+      if (!web3auth) {
+        uiConsole("web3auth not initialized yet");
+        return;
+      }
+      const idToken = await web3auth.authenticateUser();
+      uiConsole(idToken);
+    };
 
-		const authenticateUser = async () => {
-			if (!web3auth) {
-				uiConsole('web3auth not initialized yet');
-				return;
-			}
-			const idToken = await web3auth.authenticateUser();
-			uiConsole(idToken);
-		};
+    const getUserInfo = async () => {
+      if (!web3auth) {
+        uiConsole("web3auth not initialized yet");
+        return;
+      }
+      const user = await web3auth.getUserInfo();
+      uiConsole(user);
+    };
 
-		const getUserInfo = async () => {
-			if (!web3auth) {
-				uiConsole('web3auth not initialized yet');
-				return;
-			}
-			const user = await web3auth.getUserInfo();
-			uiConsole(user);
-		};
+    const logout = async () => {
+      if (!web3auth) {
+        uiConsole("web3auth not initialized yet");
+        return;
+      }
+      await web3auth.logout();
+      provider = null;
+      loggedin.value = false;
+    };
 
-		const logout = async () => {
-			if (!web3auth) {
-				uiConsole('web3auth not initialized yet');
-				return;
-			}
-			await web3auth.logout();
-			provider = null;
-			loggedin.value = false;
-		};
+    const onGetStarkAccount = async () => {
+      if (!provider) {
+        uiConsole("provider not initialized yet");
+        return;
+      }
+      const rpc = new RPC(provider as SafeEventEmitterProvider);
+      const starkaccounts = await rpc.getStarkAccount();
+      uiConsole(starkaccounts);
+    };
 
-		const onGetStarkAccount = async () => {
-			if (!provider) {
-				uiConsole('provider not initialized yet');
-				return;
-			}
-			const rpc = new RPC(provider as SafeEventEmitterProvider);
-			const starkaccounts = await rpc.getStarkAccount();
-			uiConsole(starkaccounts);
-		};
+    const getStarkKey = async () => {
+      if (!provider) {
+        uiConsole("provider not initialized yet");
+        return;
+      }
+      const rpc = new RPC(provider as SafeEventEmitterProvider);
+      const starkKey = await rpc.getStarkKey();
+      uiConsole(starkKey);
+    };
 
-		const getStarkKey = async () => {
-			if (!provider) {
-				uiConsole('provider not initialized yet');
-				return;
-			}
-			const rpc = new RPC(provider as SafeEventEmitterProvider);
-			const starkKey = await rpc.getStarkKey();
-			uiConsole(starkKey);
-		};
+    const onDeployAccount = async () => {
+      if (!provider) {
+        uiConsole("provider not initialized yet");
+        return;
+      }
+      const rpc = new RPC(provider as SafeEventEmitterProvider);
+      const deployaccount = await rpc.deployAccount();
+      uiConsole(deployaccount);
+    };
 
-		const onDeployAccount = async () => {
-			if (!provider) {
-				uiConsole('provider not initialized yet');
-				return;
-			}
-			const rpc = new RPC(provider as SafeEventEmitterProvider);
-			const deployaccount = await rpc.deployAccount();
-			uiConsole(deployaccount);
-		};
+    function uiConsole(...args: any[]): void {
+      const el = document.querySelector("#console>p");
+      if (el) {
+        el.innerHTML = JSON.stringify(args || {}, null, 2);
+      }
+    }
 
-		function uiConsole(...args: any[]): void {
-			const el = document.querySelector('#console>p');
-			if (el) {
-				el.innerHTML = JSON.stringify(args || {}, null, 2);
-			}
-		}
-
-		return {
-			loggedin,
-			loading,
-			loginButtonStatus,
-			connecting,
-			provider,
-			web3auth,
-			login,
-			authenticateUser,
-			logout,
-			getUserInfo,
-			onGetStarkAccount,
-			getStarkKey,
-			onDeployAccount,
-		};
-	},
+    return {
+      loggedin,
+      loading,
+      loginButtonStatus,
+      connecting,
+      provider,
+      web3auth,
+      login,
+      authenticateUser,
+      logout,
+      getUserInfo,
+      onGetStarkAccount,
+      getStarkKey,
+      onDeployAccount,
+    };
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 #app {
-	width: 60%;
-	margin: auto;
-	padding: 0 2rem;
+  width: 60%;
+  margin: auto;
+  padding: 0 2rem;
 }
 h3 {
-	margin: 40px 0 0;
+  margin: 40px 0 0;
 }
 ul {
-	list-style-type: none;
-	padding: 0;
+  list-style-type: none;
+  padding: 0;
 }
 li {
-	display: inline-block;
-	margin: 0 10px;
+  display: inline-block;
+  margin: 0 10px;
 }
 a {
-	color: #42b983;
+  color: #42b983;
 }
 .card {
-	margin: 0.5rem;
-	padding: 0.7rem;
-	text-align: center;
-	color: #0070f3;
-	background-color: #fafafa;
-	text-decoration: none;
-	border: 1px solid #0070f3;
-	border-radius: 10px;
-	transition: color 0.15s ease, border-color 0.15s ease;
-	width: 100%;
+  margin: 0.5rem;
+  padding: 0.7rem;
+  text-align: center;
+  color: #0070f3;
+  background-color: #fafafa;
+  text-decoration: none;
+  border: 1px solid #0070f3;
+  border-radius: 10px;
+  transition: color 0.15s ease, border-color 0.15s ease;
+  width: 100%;
 }
 
 .card:hover,
 .card:focus,
 .card:active {
-	cursor: pointer;
-	background-color: #f1f1f1;
+  cursor: pointer;
+  background-color: #f1f1f1;
 }
 
 .flex-container {
-	display: flex;
-	flex-flow: row wrap;
+  display: flex;
+  flex-flow: row wrap;
 }
 
 .flex-container > div {
-	width: 100px;
-	margin: 10px;
-	text-align: center;
-	line-height: 75px;
-	font-size: 30px;
+  width: 100px;
+  margin: 10px;
+  text-align: center;
+  line-height: 75px;
+  font-size: 30px;
 }
 
 #console {
-	width: 100%;
-	height: 100%;
-	overflow: auto;
-	word-wrap: break-word;
-	font-size: 16px;
-	font-family: monospace;
-	text-align: left;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  word-wrap: break-word;
+  font-size: 16px;
+  font-family: monospace;
+  text-align: left;
 }
 </style>

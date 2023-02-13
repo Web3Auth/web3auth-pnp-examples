bb4df65edc1bd62f2b981fd293fcbf9b796ac275
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2023-02-13 15:25:56 +0530

    Remove agent base from sfa example

--- web-single-factor-auth/react-evm-sfa-example/config-overrides.js
+++ web-single-factor-auth/react-evm-sfa-example/config-overrides.js
@@ -7,7 +7,7 @@ module.exports = function override(config) {
     stream: require.resolve("stream-browserify"),
     assert: require.resolve("assert"),
     http: require.resolve("stream-http"),
-    https: require.resolve("agent-base"),
+    https: require.resolve("https-browserify"),
     os: require.resolve("os-browserify"),
     url: require.resolve("url"),
   });

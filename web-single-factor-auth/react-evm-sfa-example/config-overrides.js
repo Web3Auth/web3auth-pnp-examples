40c2ce32e6b003b879a61f13ee42c0c61234f172
Thant Sin Oo ( Howard Chang ) <howard@unh3o.com>
2023-05-26 20:36:05 +0800

    Fix webpack polyfill

--- web-single-factor-auth/react-evm-sfa-example/config-overrides.js
+++ web-single-factor-auth/react-evm-sfa-example/config-overrides.js
@@ -10,6 +10,7 @@ module.exports = function override(config) {
     https: require.resolve("https-browserify"),
     os: require.resolve("os-browserify"),
     url: require.resolve("url"),
+    zlib: require.resolve("browserify-zlib"),
   });
   config.resolve.fallback = fallback;
   config.plugins = (config.plugins || []).concat([

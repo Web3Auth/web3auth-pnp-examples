6085a6b698d7c7e555c7fb22bbb60923c5b0f079
Maharshi Mishra <ihsraham27@gmail.com>
2023-06-09 23:47:06 +0530

    starknet tested

--- web-no-modal-sdk/starknet/react-starknet-no-modal-example/src/App.tsx
+++ web-no-modal-sdk/starknet/react-starknet-no-modal-example/src/App.tsx
@@ -5,6 +5,7 @@ import {
   SafeEventEmitterProvider,
   WALLET_ADAPTERS,
 } from "@web3auth/base";
+import { CommonPrivateKeyProvider } from "@web3auth/base-provider";
 import { OpenloginAdapter } from "@web3auth/openlogin-adapter";
 import RPC from "./starknetRPC";
 import "./App.css";
@@ -17,25 +18,37 @@ function App() {
   const [provider, setProvider] = useState<SafeEventEmitterProvider | null>(
     null
   );
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
 
   useEffect(() => {
     const init = async () => {
       try {
-        const web3auth = new Web3AuthNoModal({
+        const chainConfig = {
+          chainNamespace: CHAIN_NAMESPACES.OTHER,
+          chainId: "0x1",
+          rpcTarget: "https://rpc.ankr.com/eth",
+          displayName: "Ethereum Mainnet",
+          blockExplorer: "https://goerli.etherscan.io",
+          ticker: "ETH",
+          tickerName: "Ethereum",
+        };
+        const web3authInstance = new Web3AuthNoModal({
           clientId,
-          chainConfig: {
-            chainNamespace: CHAIN_NAMESPACES.OTHER,
-          },
+          chainConfig,
           web3AuthNetwork: "cyan",
         });
 
-        const openloginAdapter = new OpenloginAdapter();
-        web3auth.configureAdapter(openloginAdapter);
-        setWeb3auth(web3auth);
+        const privateKeyProvider = new CommonPrivateKeyProvider();
+        const openloginAdapter = new OpenloginAdapter({
+          privateKeyProvider,
+        });
+        web3authInstance.configureAdapter(openloginAdapter);
+        setWeb3auth(web3authInstance);
 
-        await web3auth.init();
-        if (web3auth.provider) {
-          setProvider(web3auth.provider);
+        await web3authInstance.init();
+        setProvider(web3authInstance.provider);
+        if (web3authInstance.connectedAdapterName) {
+          setLoggedIn(true);
         }
       } catch (error) {
         console.error(error);
@@ -57,6 +70,7 @@ function App() {
       }
     );
     setProvider(web3authProvider);
+    setLoggedIn(true);
   };
 
   const authenticateUser = async () => {
@@ -84,6 +98,7 @@ function App() {
     }
     await web3auth.logout();
     setProvider(null);
+    setLoggedIn(false);
   };
 
   const onGetStarkAccount = async () => {
@@ -179,7 +194,7 @@ function App() {
         & ReactJS StarkNet Example
       </h1>
 
-      <div className="grid">{provider ? loggedInView : unloggedInView}</div>
+      <div className="grid">{loggedIn ? loggedInView : unloggedInView}</div>
 
       <footer className="footer">
         <a

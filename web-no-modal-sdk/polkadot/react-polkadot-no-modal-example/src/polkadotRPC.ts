d59c553fbdd4291a8ad9662852bd8c6060cb729e
Maharshi Mishra <ihsraham27@gmail.com>
2023-06-09 06:20:48 +0530

    Polkadot working

--- web-no-modal-sdk/polkadot/react-polkadot-no-modal-example/src/polkadotRPC.ts
+++ web-no-modal-sdk/polkadot/react-polkadot-no-modal-example/src/polkadotRPC.ts
@@ -1,10 +1,10 @@
-import { SafeEventEmitterProvider } from "@web3auth/base";
-import { Keyring } from "@polkadot/api";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-console */
+import { ApiPromise, Keyring, WsProvider } from "@polkadot/api";
 import { cryptoWaitReady } from "@polkadot/util-crypto";
+import { SafeEventEmitterProvider } from "@web3auth/base";
 
-import { ApiPromise, WsProvider } from "@polkadot/api";
-
-export default class Polkadot {
+export default class PolkadotRPC {
   private provider: SafeEventEmitterProvider;
 
   constructor(provider: SafeEventEmitterProvider) {
@@ -26,10 +26,10 @@ export default class Polkadot {
     const privateKey = (await this.provider.request({
       method: "private_key",
     })) as string;
-    console.log("privateKey", "0x" + privateKey);
+    console.log("privateKey", `0x${privateKey}`);
     const keyring = new Keyring({ ss58Format: 42, type: "sr25519" });
 
-    const keyPair = keyring.addFromUri("0x" + privateKey);
+    const keyPair = keyring.addFromUri(`0x${privateKey}`);
     console.log("keyPair", keyPair);
     return keyPair;
   };
@@ -48,12 +48,14 @@ export default class Polkadot {
   };
 
   signAndSendTransaction = async (): Promise<any> => {
-    const keyPair = await this.getPolkadotKeyPair();
-    const api = await this.makeClient();
-    const txHash = await api.tx.balances
-      .transfer("5Gzhnn1MsDUjMi7S4cN41CfggEVzSyM58LkTYPFJY3wt7o3d", 12345)
-      .signAndSend(keyPair);
-    console.log(txHash);
-    return txHash.toHuman();
+    try {
+      const keyPair = await this.getPolkadotKeyPair();
+      const api = await this.makeClient();
+      const txHash = await api.tx.balances.transfer("5Gzhnn1MsDUjMi7S4cN41CfggEVzSyM58LkTYPFJY3wt7o3d", 12345).signAndSend(keyPair);
+      console.log(txHash);
+      return txHash.toHuman();
+    } catch (err: any) {
+      return err.toString();
+    }
   };
 }

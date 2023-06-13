73f37e3614ac7596a700f78a370568ffba057791
Maharshi Mishra <ihsraham27@gmail.com>
2023-06-13 14:14:31 +0530

    tezos react fixed, able to send transaction

--- web-no-modal-sdk/tezos/react-tezos-no-modal-example/config-overrides.js
+++ web-no-modal-sdk/tezos/react-tezos-no-modal-example/config-overrides.js
@@ -4,7 +4,7 @@ module.exports = function override(config) {
   const fallback = config.resolve.fallback || {};
   Object.assign(fallback, {
     crypto: false,
-    stream: false,
+    stream: require.resolve("stream-browserify"),
     assert: false,
     http: false,
     https: false,

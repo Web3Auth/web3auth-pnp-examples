226c33756e24237987adb9fedb37f3140f0b7e27
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2023-07-22 23:55:41 +0530

    Replace older example

--- react-native/rn-bare-firebase-example/index.js
+++ react-native/rn-bare-firebase-example/index.js
@@ -3,7 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
+import './globals';
 import App from './App';
 import {name as appName} from './app.json';
-
+console.log('index.js');
 AppRegistry.registerComponent(appName, () => App);

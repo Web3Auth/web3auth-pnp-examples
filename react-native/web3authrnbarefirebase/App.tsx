6cfbc51ba48ba96291e3c0971af3698eb1ecd55e
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2023-07-22 23:54:12 +0530

    Working firebase rn app

--- react-native/web3authrnbarefirebase/App.tsx
+++ react-native/web3authrnbarefirebase/App.tsx
@@ -1,118 +1,228 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
+  Button,
+  ScrollView,
+  Dimensions,
 } from 'react-native';
+import * as WebBrowser from '@toruslabs/react-native-web-browser';
+import EncryptedStorage from 'react-native-encrypted-storage';
+import Web3Auth, {
+  LOGIN_PROVIDER,
+  OPENLOGIN_NETWORK,
+  OpenloginUserInfo,
+} from '@web3auth/react-native-sdk';
+import RPC from './ethersRPC'; // for using ethers.js
+import firebaseAuth from '@react-native-firebase/auth';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const scheme = 'web3authrnbarefirebase'; // Or your desired app redirection scheme
+const resolvedRedirectUrl = `${scheme}://openlogin`;
+const clientId =
+  'BEglQSgt4cUWcj6SKRdu5QkOXTsePmMcusG5EAoyjyOYKlVRjIF1iCNnMOTfpzCiunHRrMui8TIwQPXdkQ8Yxuk';
+const web3auth = new Web3Auth(WebBrowser, EncryptedStorage, {
+  clientId,
+  network: OPENLOGIN_NETWORK.CYAN, // or other networks
+  useCoreKitKey: false,
+  loginConfig: {
+    jwt: {
+      name: 'Web3Auth-Auth0-JWT',
+      verifier: 'web3auth-firebase-examples',
+      typeOfLogin: 'jwt',
+      clientId,
+    },
+  },
+});
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+async function signInWithEmailPassword() {
+  try {
+    const res = await firebaseAuth().signInWithEmailAndPassword(
+      'custom+id_token@firebase.login',
+      'Welcome@W3A',
+    );
+    return res;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+export default function App() {
+  const [userInfo, setUserInfo] = useState<OpenloginUserInfo | undefined>();
+  const [key, setKey] = useState<string | undefined>('');
+  const [console, setConsole] = useState<string>('');
+
+  const login = async () => {
+    try {
+      if (!web3auth) {
+        setConsole('Web3auth not initialized');
+        return;
+      }
+
+      setConsole('Logging in');
+      const loginRes = await signInWithEmailPassword();
+      uiConsole('Login success', loginRes);
+      const idToken = await loginRes?.user.getIdToken(true);
+      uiConsole('idToken', idToken);
+      await web3auth.login({
+        loginProvider: LOGIN_PROVIDER.JWT,
+        redirectUrl: resolvedRedirectUrl,
+        mfaLevel: 'default',
+        curve: 'secp256k1',
+        extraLoginOptions: {
+          id_token: idToken,
+          verifierIdField: 'sub',
+          domain: 'http://localhost:3000',
+        },
+      });
+      setConsole(`Logged in ${web3auth.privKey}`);
+      if (web3auth.privKey) {
+        setUserInfo(web3auth.userInfo());
+        setKey(web3auth.privKey);
+        uiConsole('Logged In');
+      }
+    } catch (e: any) {
+      setConsole(e.message);
+    }
+  };
+
+  const logout = async () => {
+    if (!web3auth) {
+      setConsole('Web3auth not initialized');
+      return;
+    }
+
+    setConsole('Logging out');
+    await web3auth.logout();
+
+    if (!web3auth.privKey) {
+      setUserInfo(undefined);
+      setKey('');
+      uiConsole('Logged out');
+    }
+  };
+
+  useEffect(() => {
+    const init = async () => {
+      await web3auth.init();
+      if (web3auth?.privKey) {
+        uiConsole('Re logged in');
+        setUserInfo(web3auth.userInfo());
+        setKey(web3auth.privKey);
+        uiConsole(web3auth.privKey);
+      }
+    };
+    init();
+  }, []);
+
+  const getChainId = async () => {
+    setConsole('Getting chain id');
+    const networkDetails = await RPC.getChainId();
+    uiConsole(networkDetails);
+  };
+
+  const getAccounts = async () => {
+    if (!key) {
+      setConsole('User not logged in');
+      return;
+    }
+    setConsole('Getting account');
+    const address = await RPC.getAccounts(key);
+    uiConsole(address);
+  };
+  const getBalance = async () => {
+    if (!key) {
+      setConsole('User not logged in');
+      return;
+    }
+    setConsole('Fetching balance');
+    const balance = await RPC.getBalance(key);
+    uiConsole(balance);
+  };
+  const sendTransaction = async () => {
+    if (!key) {
+      setConsole('User not logged in');
+      return;
+    }
+    setConsole('Sending transaction');
+    const tx = await RPC.sendTransaction(key);
+    uiConsole(tx);
+  };
+  const signMessage = async () => {
+    if (!key) {
+      setConsole('User not logged in');
+      return;
+    }
+    setConsole('Signing message');
+    const message = await RPC.signMessage(key);
+    uiConsole(message);
+  };
+
+  const uiConsole = (...args: unknown[]) => {
+    setConsole(JSON.stringify(args || {}, null, 2) + '\n\n\n\n' + console);
+  };
+
+  const loggedInView = (
+    <View style={styles.buttonArea}>
+      <Button title="Get User Info" onPress={() => uiConsole(userInfo)} />
+      <Button title="Get Chain ID" onPress={() => getChainId()} />
+      <Button title="Get Accounts" onPress={() => getAccounts()} />
+      <Button title="Get Balance" onPress={() => getBalance()} />
+      <Button title="Send Transaction" onPress={() => sendTransaction()} />
+      <Button title="Sign Message" onPress={() => signMessage()} />
+      <Button title="Get Private Key" onPress={() => uiConsole(key)} />
+      <Button title="Log Out" onPress={logout} />
+    </View>
+  );
+
+  const unloggedInView = (
+    <View style={styles.buttonArea}>
+      <Button title="Login with Web3Auth" onPress={login} />
+    </View>
+  );
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      {key ? loggedInView : unloggedInView}
+      <View style={styles.consoleArea}>
+        <Text style={styles.consoleText}>Console:</Text>
+        <ScrollView style={styles.console}>
+          <Text>{console}</Text>
+        </ScrollView>
+      </View>
     </View>
   );
 }
 
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 50,
+    paddingBottom: 30,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  consoleArea: {
+    margin: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  console: {
+    flex: 1,
+    backgroundColor: '#CCCCCC',
+    color: '#ffffff',
+    padding: 10,
+    width: Dimensions.get('window').width - 60,
   },
-  highlight: {
-    fontWeight: '700',
+  consoleText: {
+    padding: 10,
+  },
+  buttonArea: {
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    paddingBottom: 30,
   },
 });
-
-export default App;
--- react-native/web3authrnbarefirebase/metro.config.js
+++ react-native/web3authrnbarefirebase/metro.config.js
@@ -1,11 +1,40 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+// Learn more https://docs.expo.io/guides/customizing-metro
+const {getDefaultConfig} = require('metro-config');
 
-/**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
+module.exports = (async () => {
+  const {
+    resolver: {sourceExts, assetExts},
+  } = await getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+  const defaultSourceExts = [...sourceExts, 'svg', 'mjs', 'cjs'];
+
+  return {
+    resolver: {
+      extraNodeModules: {
+        assert: require.resolve('empty-module'), // assert can be polyfilled here if needed
+        http: require.resolve('empty-module'), // stream-http can be polyfilled here if needed
+        https: require.resolve('empty-module'), // https-browserify can be polyfilled here if needed
+        os: require.resolve('empty-module'), // os-browserify can be polyfilled here if needed
+        url: require.resolve('empty-module'), // url can be polyfilled here if needed
+        zlib: require.resolve('empty-module'), // browserify-zlib can be polyfilled here if needed
+        path: require.resolve('empty-module'),
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('readable-stream'),
+      },
+
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+
+      sourceExts: process.env.TEST_REACT_NATIVE
+        ? ['e2e.js'].concat(defaultSourceExts)
+        : defaultSourceExts,
+    },
+    transformer: {
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+    },
+  };
+})();

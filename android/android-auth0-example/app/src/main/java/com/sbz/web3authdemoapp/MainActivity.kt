4f1a54993bf28dd1230c3abdf1a43d6dde84b4d4
Mohammad Shahbaz Alam <shahbaz17@users.noreply.github.com>
2023-10-12 08:21:09 +0530

    Update version to latest

--- android/android-auth0-example/app/src/main/java/com/sbz/web3authdemoapp/MainActivity.kt
+++ android/android-auth0-example/app/src/main/java/com/sbz/web3authdemoapp/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity() {
                context = this,
                clientId = getString(R.string.web3auth_project_id), // pass over your Web3Auth Client ID from Developer Dashboard
                network = Network.SAPPHIRE_MAINNET, // pass over the network you want to use (MAINNET or TESTNET or CYAN)
-               buildEnv = BuildEnv.TESTING,
+               buildEnv = BuildEnv.PRODUCTION,
                redirectUrl = Uri.parse("com.sbz.web3authdemoapp://auth"), // your app's redirect URL
                // Optional parameters
                whiteLabel = WhiteLabelData(

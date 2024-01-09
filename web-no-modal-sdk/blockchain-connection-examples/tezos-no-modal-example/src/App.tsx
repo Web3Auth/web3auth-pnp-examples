b9fab2e473acc0e38ccba713af92462ed6eef825
Maharshi Mishra <maharshi@tor.us>
2024-01-09 16:18:02 +0530

    added double quotes to path

--- web-no-modal-sdk/blockchain-connection-examples/tezos-no-modal-example/src/App.tsx
+++ web-no-modal-sdk/blockchain-connection-examples/tezos-no-modal-example/src/App.tsx
@@ -222,7 +222,7 @@ function App() {
 
       <footer className="footer">
         <a
-          href="https://github.com/Web3Auth/web3auth-pnp-examples/tree/main/web-no-modal-sdk/blockchain-connection-examples/tezos-no-modal-example
+          href="https://github.com/Web3Auth/web3auth-pnp-examples/tree/main/web-no-modal-sdk/blockchain-connection-examples/tezos-no-modal-example"
           target="_blank"
           rel="noopener noreferrer"
         >

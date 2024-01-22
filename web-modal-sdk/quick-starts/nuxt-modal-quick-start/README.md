ff3bb800a45c820f4956c4290c76841013e2044b
Tomas Rawski <rtomas@gmail.com>
2024-01-21 22:32:24 -0300

    first commit for nuxt quickstart

---
+++ web-modal-sdk/quick-starts/nuxt-modal-quick-start/README.md
@@ -0,0 +1,40 @@
+# Web3Auth (`@web3auth/modal`) x EVM x Nuxt
+
+[![Web3Auth](https://img.shields.io/badge/Web3Auth-SDK-blue)](https://web3auth.io/docs/sdk/pnp/web/modal)
+[![Web3Auth](https://img.shields.io/badge/Web3Auth-Community-cyan)](https://community.web3auth.io)
+
+[Join our Community Portal](https://community.web3auth.io/) to get support and stay up to date with the latest news and updates.
+
+This example demonstrates how to use Web3Auth with EVM in Nuxt.
+
+## Setup
+
+Make sure to install the dependencies:
+
+```bash
+# npm
+npm install
+
+# pnpm
+pnpm install
+
+# yarn
+yarn install
+
+```
+
+## Development Server
+
+Start the development server on `http://localhost:3000`:
+
+```bash
+# npm
+npm run dev
+
+# pnpm
+pnpm run dev
+
+# yarn
+yarn dev
+
+```
---
+++ web-modal-sdk/quick-starts/nuxt-modal-quick-start/components/Home.vue
@@ -0,0 +1,202 @@
+<template>
+  <div id="app">
+    <h2>
+      <a target="_blank" href="https://web3auth.io/docs/sdk/pnp/web/modal" rel="noreferrer"> Web3Auth </a>
+      Nuxt Quick Start
+    </h2>
+
+    <button v-if="!loggedIn" class="card" @click="login" style="cursor: pointer">Login</button>
+
+    <div v-if="loggedIn">
+      <div class="flex-container">
+        <div>
+          <button class="card" @click="getUserInfo" style="cursor: pointer">Get User Info</button>
+        </div>
+        <div>
+          <button class="card" @click="getAccounts" style="cursor: pointer">Get Accounts</button>
+        </div>
+        <div>
+          <button class="card" @click="getBalance" style="cursor: pointer">Get Balance</button>
+        </div>
+        <div>
+          <button class="card" @click="signMessage" style="cursor: pointer">Sign Message</button>
+        </div>
+        <div>
+          <button class="card" @click="logout" style="cursor: pointer">Logout</button>
+        </div>
+      </div>
+    </div>
+    <div id="console" style="white-space: pre-line">
+      <p style="white-space: pre-line"></p>
+    </div>
+
+    <footer class="footer">
+      <a
+        href="https://github.com/Web3Auth/web3auth-pnp-examples/tree/main/web-modal-sdk/quick-starts/nuxt-modal-quick-start"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Source code
+      </a>
+    </footer>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref, onMounted } from "vue";
+// IMP START - Quick Start
+import { Web3Auth } from "@web3auth/modal";
+import { CHAIN_NAMESPACES } from "@web3auth/base";
+import type { IProvider } from "@web3auth/base";
+// IMP END - Quick Start
+import Web3 from "web3";
+
+export default defineComponent({
+  name: "Home",
+  setup() {
+    const chainConfig = {
+      chainNamespace: CHAIN_NAMESPACES.EIP155,
+      chainId: "0x1", // Please use 0x1 for Mainnet
+      rpcTarget: "https://rpc.ankr.com/eth",
+      displayName: "Ethereum Mainnet",
+      blockExplorer: "https://etherscan.io/",
+      ticker: "ETH",
+      tickerName: "Ethereum",
+    };
+
+    const clientId = "BPi5PB_UiIZ-cPz1GtV5i1I2iOSOHuimiXBI0e-Oe_u6X3oVAbCiAZOTEBtTXw4tsluTITPqA8zMsfxIKMjiqNQ"; // get from https://dashboard.web3auth.io
+
+    const web3auth = new Web3Auth({
+      clientId,
+      chainConfig,
+      web3AuthNetwork: "sapphire_mainnet",
+    });
+
+    const loggedIn = ref<boolean>(false);
+    let provider = <IProvider | null>null;
+
+    // IMP END - SDK Initialization
+
+    onMounted(async () => {
+      const init = async () => {
+        try {
+          // IMP START - SDK Initialization
+          await web3auth.initModal();
+          // IMP END - SDK Initialization
+          provider = web3auth.provider;
+
+          if (web3auth.connected) {
+            loggedIn.value = true;
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      };
+
+      init();
+    });
+
+    const login = async () => {
+      // IMP START - Login
+      provider = await web3auth.connect();
+      // IMP END - Login
+      if (web3auth.connected) {
+        loggedIn.value = true;
+      }
+    };
+
+    const getUserInfo = async () => {
+      // IMP START - Get User Information
+      const user = await web3auth.getUserInfo();
+      // IMP END - Get User Information
+      uiConsole(user);
+    };
+
+    const logout = async () => {
+      // IMP START - Logout
+      await web3auth.logout();
+      // IMP END - Logout
+      provider = null;
+      loggedIn.value = false;
+      uiConsole("logged out");
+    };
+
+    // IMP START - Blockchain Calls
+    const getAccounts = async () => {
+      if (!provider) {
+        uiConsole("provider not initialized yet");
+        return;
+      }
+      const web3 = new Web3(provider as any);
+
+      // Get user's Ethereum public address
+      const address = await web3.eth.getAccounts();
+      uiConsole(address);
+    };
+
+    const getBalance = async () => {
+      if (!provider) {
+        uiConsole("provider not initialized yet");
+        return;
+      }
+      const web3 = new Web3(provider as any);
+
+      // Get user's Ethereum public address
+      const address = (await web3.eth.getAccounts())[0];
+
+      // Get user's balance in ether
+      const balance = web3.utils.fromWei(
+        await web3.eth.getBalance(address), // Balance is in wei
+        "ether"
+      );
+      uiConsole(balance);
+    };
+
+    const signMessage = async () => {
+      if (!provider) {
+        uiConsole("provider not initialized yet");
+        return;
+      }
+      const web3 = new Web3(provider as any);
+
+      // Get user's Ethereum public address
+      const fromAddress = (await web3.eth.getAccounts())[0];
+
+      const originalMessage = "YOUR_MESSAGE";
+
+      // Sign the message
+      const signedMessage = await web3.eth.personal.sign(
+        originalMessage,
+        fromAddress,
+        "test password!" // configure your own password here.
+      );
+      uiConsole(signedMessage);
+    };
+    // IMP END - Blockchain Calls
+
+    function uiConsole(...args: any[]): void {
+      const el = document.querySelector("#console>p");
+      if (el) {
+        el.innerHTML = JSON.stringify(args || {}, null, 2);
+      }
+      console.log(...args);
+    }
+
+    return {
+      loggedIn,
+      provider,
+      web3auth,
+      login,
+      logout,
+      getUserInfo,
+      getAccounts,
+      getBalance,
+      signMessage,
+    };
+  },
+});
+</script>
+
+<style scoped>
+/* Add your component-specific styles here */
+</style>

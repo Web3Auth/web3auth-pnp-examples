12fcca3cf3c1564cc3d6a9e0181323e54f26a4fd
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2024-01-25 17:21:34 +0530

    improve initialization of web3Auth

--- ios/ios-quick-start/ios-example/ViewModel.swift
+++ ios/ios-quick-start/ios-example/ViewModel.swift
@@ -21,8 +21,11 @@ class ViewModel: ObservableObject {
             isLoading = true
             navigationTitle = "Loading"
         })
+        
         // IMP START - Initialize Web3Auth
-        web3Auth = await Web3Auth(.init(clientId: clientId, network: network))
+        web3Auth = await Web3Auth(W3AInitParams(
+            clientId: clientId, network: network
+        ))
         // IMP END - Initialize Web3Auth
         await MainActor.run(body: {
             if self.web3Auth?.state != nil {
@@ -33,18 +36,20 @@ class ViewModel: ObservableObject {
             navigationTitle = loggedIn ? "UserInfo" : "SignIn"
         })
     }
-
+    
     func login(provider: Web3AuthProvider) {
         Task {
             do {
                 // IMP START - Login
-                let result = try await Web3Auth(.init(clientId: clientId, network: network)).login(W3ALoginParams(loginProvider: provider))
+                let result = try await web3Auth?.login(
+                    W3ALoginParams(loginProvider: provider)
+                )
                 // IMP END - Login
                 await MainActor.run(body: {
                     user = result
                     loggedIn = true
                 })
-
+                
             } catch {
                 print("Error")
             }
@@ -54,9 +59,7 @@ class ViewModel: ObservableObject {
     func logout() throws {
         Task {
             // IMP START - Logout
-            try await Web3Auth(.init(
-                clientId: clientId, network: .sapphire_mainnet
-            )).logout()
+            try await web3Auth?.logout()
             // IMP END - Logout
             await MainActor.run(body: {
                 loggedIn = false
@@ -68,14 +71,14 @@ class ViewModel: ObservableObject {
         Task {
             do {
                 // IMP START - Login
-                let result = try await Web3Auth(.init(clientId: clientId, network: network)).login(W3ALoginParams(loginProvider: provider, extraLoginOptions: ExtraLoginOptions(display: nil, prompt: nil, max_age: nil, ui_locales: nil, id_token_hint: nil, id_token: nil, login_hint: email, acr_values: nil, scope: nil, audience: nil, connection: nil, domain: nil, client_id: nil, redirect_uri: nil, leeway: nil, verifierIdField: nil, isVerifierIdCaseSensitive: nil, additionalParams: nil)))
+                let result = try await web3Auth?.login(W3ALoginParams(loginProvider: provider, extraLoginOptions: ExtraLoginOptions(display: nil, prompt: nil, max_age: nil, ui_locales: nil, id_token_hint: nil, id_token: nil, login_hint: email, acr_values: nil, scope: nil, audience: nil, connection: nil, domain: nil, client_id: nil, redirect_uri: nil, leeway: nil, verifierIdField: nil, isVerifierIdCaseSensitive: nil, additionalParams: nil)))
                 // IMP END - Login
                 await MainActor.run(body: {
                     user = result
                     loggedIn = true
                     navigationTitle = "UserInfo"
                 })
-
+                
             } catch {
                 print("Error")
             }

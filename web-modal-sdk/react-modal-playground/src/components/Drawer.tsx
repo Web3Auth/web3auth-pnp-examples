86aa0aaca28cfa9939b5a0dd763d06e9a1c9241e
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2024-03-21 13:33:06 -0700

    Add source code to sidebar and drawer

--- web-modal-sdk/react-modal-playground/src/components/Drawer.tsx
+++ web-modal-sdk/react-modal-playground/src/components/Drawer.tsx
@@ -9,7 +9,7 @@ interface DrawerProps {
   setOpen: any;
 }
 const Drawer = ({ isOpen, setOpen }: DrawerProps) => {
-  const { logout } = useWeb3Auth();
+  const { logout, connectedChain } = useWeb3Auth();
 
   const navigate = useNavigate();
   function goToHome() {
@@ -24,25 +24,38 @@ const Drawer = ({ isOpen, setOpen }: DrawerProps) => {
   function goToServerSideVerification() {
     navigate("/server-side-verification");
   }
+  function goToExplorer() {
+    window.open(connectedChain.blockExplorerUrl);
+  }
+  function goToFaucet() {
+    if (connectedChain.chainId === "0xaa36a7") {
+      window.open("https://www.infura.io/faucet/sepolia");
+    } else if (connectedChain.chainId === "0x13881") {
+      window.open("https://faucet.polygon.technology/");
+    }
+  }
+  function goToSounceCode() {
+    window.open("https://github.com/Web3Auth/web3auth-pnp-examples/tree/main/web-modal-sdk/react-modal-playground");
+  }
   const location = useLocation();
-  function linktoGo(label: string, path: any) {
+  function linktoGo(label: string, path: any, id: number) {
     return (
       <div
         onClick={() => path()}
+        key={id}
         className="flex items-center px-4 py-2 mb-2 text-gray-500 rounded-lg hover:bg-gray-100 hover:text-primary  cursor-pointer"
       >
         <span className="text-sm font-normal">{label}</span>
       </div>
     );
   }
-  function activePage(label: string) {
+  function activePage(label: string, id: number) {
     return (
-      <div className="flex items-center px-4 py-2 mb-2 rounded-lg bg-gray-100 text-primary  cursor-pointer">
+      <div key={id} className="flex items-center px-4 py-2 mb-2 rounded-lg bg-gray-100 text-primary cursor-pointer">
         <span className="text-sm font-bold">{label}</span>
       </div>
     );
   }
-
   if (isOpen) {
     return (
       <div className="fixed flex w-full h-full  lg:hidden">
@@ -51,12 +64,17 @@ const Drawer = ({ isOpen, setOpen }: DrawerProps) => {
           <div className="py-2">
             <strong className="px-4 block p-1 text-xs font-medium text-gray-400 uppercase">MENU</strong>
             <nav className="flex flex-col mt-6">
-              {location.pathname === "/" ? activePage("Main Page") : linktoGo("Main Page", goToHome)}
-              {location.pathname === "/transaction" ? activePage("Transactions") : linktoGo("Transactions", goToTransaction)}
-              {location.pathname === "/contract" ? activePage("Smart Contract Interactions") : linktoGo("Smart Contract Interactions", goToContract)}
+              {location.pathname === "/" ? activePage("Main Page", 1) : linktoGo("Main Page", goToHome, 1)}
+              {location.pathname === "/transaction" ? activePage("Transactions", 2) : linktoGo("Transactions", goToTransaction, 2)}
+              {location.pathname === "/contract"
+                ? activePage("Smart Contract Interactions", 3)
+                : linktoGo("Smart Contract Interactions", goToContract, 3)}
               {location.pathname === "/server-side-verification"
-                ? activePage("Server Side Verification")
-                : linktoGo("Server Side Verification", goToServerSideVerification)}
+                ? activePage("Server Side Verification", 4)
+                : linktoGo("Server Side Verification", goToServerSideVerification, 4)}
+              {linktoGo("Explorer Link", goToExplorer, 5)}
+              {connectedChain.chainId === "0x5" || connectedChain.chainId === "0x13881" ? linktoGo("Faucet Link", goToFaucet, 6) : null}
+              {linktoGo("Source Code", goToSounceCode, 6)}
               <div
                 onClick={() => {
                   setOpen(false);

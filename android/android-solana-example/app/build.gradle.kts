5c6295c89b010d09469b11616361b1c30fcbe2d3
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2024-04-04 16:20:08 +0530

    update to latest version

--- android/android-solana-example/app/build.gradle.kts
+++ android/android-solana-example/app/build.gradle.kts
@@ -64,7 +64,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("com.github.Web3Auth:web3auth-android-sdk:7.0.0")
+    implementation("com.github.Web3Auth:web3auth-android-sdk:7.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")

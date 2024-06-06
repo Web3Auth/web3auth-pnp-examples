7731e73319f2bdf41bd4920cd4656cdc2b8de946
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2024-06-07 04:49:15 +0530

    Update rn-bare-auth0-example

--- react-native/rn-bare-auth0-example/index.js
+++ react-native/rn-bare-auth0-example/index.js
@@ -1,10 +1,6 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import './globals';
-import 'react-native-get-random-values';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from "react-native";
+import "react-native-get-random-values";
+import "./globals";
+import App from "./App";
+import { name as appName } from "./app.json";
 AppRegistry.registerComponent(appName, () => App);
--- react-native/rn-bare-auth0-example/metro.config.js
+++ react-native/rn-bare-auth0-example/metro.config.js
@@ -1,38 +1,22 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require("metro-config");
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
-module.exports = (async () => {
-    const {
-        resolver: { sourceExts, assetExts },
-    } = await getDefaultConfig(__dirname);
+const defaultConfig = getDefaultConfig(__dirname);
 
-    const defaultSourceExts = [...sourceExts, "svg", "mjs", "cjs"];
-
-    return {
-        resolver: {
-            extraNodeModules: {
-                assert: require.resolve("empty-module"), // assert can be polyfilled here if needed
-                http: require.resolve("empty-module"), // stream-http can be polyfilled here if needed
-                https: require.resolve("empty-module"), // https-browserify can be polyfilled here if needed
-                os: require.resolve("empty-module"), // os-browserify can be polyfilled here if needed
-                url: require.resolve("empty-module"), // url can be polyfilled here if needed
-                zlib: require.resolve("empty-module"), // browserify-zlib can be polyfilled here if needed
-                path: require.resolve("empty-module"),
-                crypto: require.resolve("react-native-quick-crypto"),
-                stream: require.resolve("readable-stream"),
-            },
-
-            assetExts: assetExts.filter((ext) => ext !== "svg"),
-
-            sourceExts: process.env.TEST_REACT_NATIVE ? ["e2e.js"].concat(defaultSourceExts) : defaultSourceExts,
+const config = {
+    resolver: {
+        extraNodeModules: {
+            assert: require.resolve("empty-module"), // assert can be polyfilled here if needed
+            http: require.resolve("empty-module"), // stream-http can be polyfilled here if needed
+            https: require.resolve("empty-module"), // https-browserify can be polyfilled here if needed
+            os: require.resolve("empty-module"), // os-browserify can be polyfilled here if needed
+            url: require.resolve("empty-module"), // url can be polyfilled here if needed
+            zlib: require.resolve("empty-module"), // browserify-zlib can be polyfilled here if needed
+            path: require.resolve("empty-module"),
+            crypto: require.resolve("crypto-browserify"),
+            stream: require.resolve("readable-stream"),
         },
-        transformer: {
-            getTransformOptions: async () => ({
-                transform: {
-                    experimentalImportSupport: false,
-                    inlineRequires: true,
-                },
-            }),
-        },
-    };
-})();
+        sourceExts: [...defaultConfig.resolver.sourceExts, 'svg'],
+    },
+};
+
+module.exports = mergeConfig(defaultConfig, config);

882dc033cec3212f4327417f5f8d30bdd4b3200d
Tomas Rawski <rtomas@gmail.com>
2024-06-10 10:30:45 -0300

    fix mfa problem. need at least 2 factors

--- web-modal-sdk/blockchain-connection-examples/xrpl-modal-example/src/App.tsx
+++ web-modal-sdk/blockchain-connection-examples/xrpl-modal-example/src/App.tsx
@@ -90,7 +90,7 @@ function App() {
               passwordFactor: {
                 enable: true,
                 priority: 4,
-                mandatory: false,
+                mandatory: true,
               },
             },
           },

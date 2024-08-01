4d79fb20018b1155a7cc5f3a47fe0fd2c1d8f1da
Yashovardhan Agrawal <21066442+yashovardhan@users.noreply.github.com>
2024-08-01 09:34:20 +0530

    Add Solana Example

---
+++ react-native/rn-bare-solana-example/App.tsx
@@ -0,0 +1,246 @@
+import React, {useEffect, useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ScrollView,
+  Dimensions,
+  TextInput,
+} from 'react-native';
+
+import * as WebBrowser from '@toruslabs/react-native-web-browser';
+import EncryptedStorage from 'react-native-encrypted-storage';
+import Web3Auth, {
+  LOGIN_PROVIDER,
+  OPENLOGIN_NETWORK,
+  ChainNamespace,
+} from '@web3auth/react-native-sdk';
+import {SolanaPrivateKeyProvider} from '@web3auth/solana-provider';
+import RPC from './solanaRPC';
+
+const scheme = 'solanarnexample'; // Or your desired app redirection scheme
+const redirectUrl = `${scheme}://openlogin`;
+
+const clientId =
+  'BPi5PB_UiIZ-cPz1GtV5i1I2iOSOHuimiXBI0e-Oe_u6X3oVAbCiAZOTEBtTXw4tsluTITPqA8zMsfxIKMjiqNQ'; // get from https://dashboard.web3auth.io
+
+const chainConfig = {
+  chainNamespace: ChainNamespace.SOLANA,
+  chainId: '0x2', // Please use 0x1 for Mainnet, 0x2 for Testnet, 0x3 for Devnet
+  rpcTarget: 'https://api.testnet.solana.com',
+  displayName: 'Solana Testnet',
+  blockExplorerUrl: 'https://explorer.solana.com',
+  ticker: 'SOL',
+  tickerName: 'Solana',
+  logo: 'https://images.toruswallet.io/solana.svg',
+};
+
+const privateKeyProvider = new SolanaPrivateKeyProvider({
+  config: {chainConfig},
+});
+
+const web3auth = new Web3Auth(WebBrowser, EncryptedStorage, {
+  clientId,
+  redirectUrl,
+  network: OPENLOGIN_NETWORK.SAPPHIRE_MAINNET, // or other networks
+});
+
+export default function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [provider, setProvider] = useState<any>(null);
+  const [console, setConsole] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+
+  useEffect(() => {
+    const init = async () => {
+      await web3auth.init();
+
+      if (web3auth.ed25519Key) {
+        await privateKeyProvider.setupProvider(web3auth.ed25519Key);
+
+        setProvider(privateKeyProvider);
+        setLoggedIn(true);
+      }
+    };
+    init();
+  }, []);
+
+  const login = async () => {
+    try {
+      if (!web3auth.ready) {
+        setConsole('Web3auth not initialized');
+        return;
+      }
+      if (!email) {
+        setConsole('Enter email first');
+        return;
+      }
+
+      setConsole('Logging in');
+
+      await web3auth.login({
+        loginProvider: LOGIN_PROVIDER.EMAIL_PASSWORDLESS,
+        extraLoginOptions: {
+          login_hint: email,
+        },
+      });
+
+      if (web3auth.ed25519Key) {
+        await privateKeyProvider.setupProvider(web3auth.ed25519Key);
+
+        setProvider(privateKeyProvider);
+        uiConsole('Logged In');
+        setLoggedIn(true);
+      }
+    } catch (e: any) {
+      setConsole(e.message);
+    }
+  };
+
+  const logout = async () => {
+    if (!web3auth.ready) {
+      setConsole('Web3auth not initialized');
+      return;
+    }
+
+    setConsole('Logging out');
+
+    await web3auth.logout();
+
+    if (!web3auth.privKey) {
+      setProvider(null);
+      uiConsole('Logged out');
+      setLoggedIn(false);
+    }
+  };
+
+  const getAccounts = async () => {
+    if (!provider) {
+      uiConsole('provider not initialized yet');
+      return;
+    }
+    const rpc = new RPC(provider);
+    const address = await rpc.getAccounts();
+    uiConsole(address);
+  };
+
+  const getBalance = async () => {
+    if (!provider) {
+      uiConsole('provider not initialized yet');
+      return;
+    }
+    const rpc = new RPC(provider);
+    const balance = await rpc.getBalance();
+    uiConsole(balance);
+  };
+
+  const signMessage = async () => {
+    if (!provider) {
+      uiConsole('provider not initialized yet');
+      return;
+    }
+    const rpc = new RPC(provider);
+    const signedMessage = await rpc.signMessage();
+    uiConsole(signedMessage);
+  };
+
+  const sendTransaction = async () => {
+    if (!provider) {
+      uiConsole('provider not initialized yet');
+      return;
+    }
+    const rpc = new RPC(provider);
+    const receipt = await rpc.sendTransaction();
+    uiConsole(receipt);
+  };
+
+  const uiConsole = (...args: unknown[]) => {
+    setConsole(JSON.stringify(args || {}, null, 2) + '\n\n\n\n' + console);
+  };
+
+  const loggedInView = (
+    <View style={styles.buttonArea}>
+      <Button
+        title="Get User Info"
+        onPress={() => uiConsole(web3auth.userInfo())}
+      />
+      <Button title="Get Accounts" onPress={() => getAccounts()} />
+      <Button title="Get Balance" onPress={() => getBalance()} />
+      <Button title="Sign Message" onPress={() => signMessage()} />
+      <Button title="Send Transaction" onPress={() => sendTransaction()} />
+      <Button title="Log Out" onPress={logout} />
+    </View>
+  );
+
+  const unloggedInView = (
+    <View style={styles.buttonAreaLogin}>
+      <TextInput
+        style={styles.inputEmail}
+        placeholder="Enter email"
+        onChangeText={setEmail}
+      />
+      <Button title="Login with Web3Auth" onPress={login} />
+    </View>
+  );
+
+  return (
+    <View style={styles.container}>
+      {loggedIn ? loggedInView : unloggedInView}
+      <View style={styles.consoleArea}>
+        <Text style={styles.consoleText}>Console:</Text>
+        <ScrollView style={styles.console}>
+          <Text>{console}</Text>
+        </ScrollView>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 50,
+    paddingBottom: 30,
+  },
+  consoleArea: {
+    margin: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+  },
+  console: {
+    flex: 1,
+    backgroundColor: '#CCCCCC',
+    color: '#ffffff',
+    padding: 10,
+    width: Dimensions.get('window').width - 60,
+  },
+  consoleText: {
+    padding: 10,
+  },
+  buttonArea: {
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    paddingBottom: 30,
+  },
+  buttonAreaLogin: {
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingBottom: 30,
+  },
+  inputEmail: {
+    height: 40,
+    width: 300,
+    borderColor: 'gray',
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 20,
+  },
+});

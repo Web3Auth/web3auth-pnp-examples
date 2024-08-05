6ee15b2afeda5dea5242489fa06430e269646e04
Mohammad Shahbaz Alam <shahbaz17@users.noreply.github.com>
2024-08-05 13:32:09 +0530

    Update wagmi example to latest
    Also added useSignTypedData usage

--- web-modal-sdk/wagmi-examples/wagmi-modal-example/src/balance.tsx
+++ web-modal-sdk/wagmi-examples/wagmi-modal-example/src/balance.tsx
@@ -1,7 +1,8 @@
-import { useAccount, useBalance } from "wagmi";
+import { useAccount, useBalance, useSignTypedData } from "wagmi";
 
 export function Balance() {
   const { address } = useAccount()
+  const { signTypedData } = useSignTypedData()
 
   const { data: default_ } = useBalance({ address })
   const { data: account_ } = useBalance({ address })
@@ -12,6 +13,40 @@ export function Balance() {
 
       <div>Balance (Default Chain): {default_?.formatted}</div>
       <div>Balance (Account Chain): {account_?.formatted}</div>
+
+      <br />
+      <button
+        onClick={() => {
+          const data = signTypedData({
+            types: {
+              Person: [
+                { name: "name", type: "string" },
+                { name: "wallet", type: "address" },
+              ],
+              Mail: [
+                { name: "from", type: "Person" },
+                { name: "to", type: "Person" },
+                { name: "contents", type: "string" },
+              ],
+            },
+            primaryType: "Mail",
+            message: {
+              from: {
+                name: "Cow",
+                wallet: "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
+              },
+              to: {
+                name: "Bob",
+                wallet: "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB",
+              },
+              contents: "Hello, Bob!",
+            },
+          });
+          console.log(data);
+        }}
+      >
+        Sign message using useSignTypedData
+      </button>
     </div>
   )
 }

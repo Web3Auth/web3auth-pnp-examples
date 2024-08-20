efe4e43f542895865249a3ea75126f8ddb8c1527
Mohammad Shahbaz Alam <shahbaz17@users.noreply.github.com>
2024-08-20 14:34:37 +0530

    add crypto as polyfill

--- web-no-modal-sdk/other/xmtp-no-modal-example/vite.config.ts
+++ web-no-modal-sdk/other/xmtp-no-modal-example/vite.config.ts
@@ -1,19 +1,18 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { resolve } from "path";
 
 // https://vitejs.dev/config/
 // IMP START - Bundler Issues
-  export default defineConfig({
+export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      // "@web3auth/ethereum-provider": resolve("./node_modules/@web3auth/ethereum-provider/dist/ethereumProvider.cjs.js"),
+      crypto: "empty-module",
     },
   },
   define: {
-    "global": "globalThis",
+    global: "globalThis",
   },
-// IMP END - Bundler Issues
+  // IMP END - Bundler Issues
 });

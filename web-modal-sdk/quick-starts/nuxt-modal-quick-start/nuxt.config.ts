5adcb2622bb27bb43f6ed82ddc5716d4f8cb9cff
Yashovardhan Agrawal <21066442+yashovardhan@users.noreply.github.com>
2024-08-26 00:46:49 +0530

    Fix nuxt build issues

--- web-modal-sdk/quick-starts/nuxt-modal-quick-start/nuxt.config.ts
+++ web-modal-sdk/quick-starts/nuxt-modal-quick-start/nuxt.config.ts
@@ -1,23 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-import { nodePolyfills } from "vite-plugin-node-polyfills";
-
 export default defineNuxtConfig({
   devtools: { enabled: true },
-
-  // IMP START - Bundler Issues
   ssr: false,
-
-  // IMP END - Bundler Issues
   vite: {
-    plugins: [
-      nodePolyfills({
-        globals: {
-          Buffer: true,
-          global: true,
-        },
-      }),
-    ],
+    define: {
+      global: 'globalThis',
+    },
   },
 
   compatibilityDate: "2024-08-08"

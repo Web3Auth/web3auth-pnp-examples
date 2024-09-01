2fff6e3f1c68c35c533e0708acaf887b85871a04
Yashovardhan Agrawal <21066442+yashovardhan@users.noreply.github.com>
2024-09-01 18:57:10 +0530

    Update unity auth0 example

--- unity/unity-auth0-example/Assets/Web3AuthScript.cs
+++ unity/unity-auth0-example/Assets/Web3AuthScript.cs
@@ -6,6 +6,7 @@ using TMPro;
 using Nethereum.Web3;
 using Nethereum.Signer;
 using Nethereum.Web3.Accounts;
+using Newtonsoft.Json.Linq;
 
 public class Web3AuthScript : MonoBehaviour
 {
@@ -41,6 +42,7 @@ public class Web3AuthScript : MonoBehaviour
         web3Auth.onLogin += onLogin;
         web3Auth.onLogout += onLogout;
         web3Auth.onMFASetup += onMFASetup;
+        web3Auth.onSignResponse += onSignResponse;
 
         updateConsole("Ready to Login!");
     }
@@ -208,6 +210,34 @@ public class Web3AuthScript : MonoBehaviour
             chainNamespace = Web3Auth.ChainNamespace.EIP155
         };
         web3Auth.launchWalletServices(chainConfig);
+    }
+
+    public void PopupSignMessageUI()
+    {
+        var chainConfig = new ChainConfig()
+        {
+            chainId = "0xaa36a7",
+            rpcTarget = "https://rpc.ankr.com/eth_sepolia",
+            ticker = "ETH",
+            chainNamespace = Web3Auth.ChainNamespace.EIP155
+        };
+
+        JArray paramsArray = new JArray
+        {
+             "Hello World",
+             account.Address,
+             "Android"
+        };
+
+        web3Auth.request(chainConfig, "personal_sign", paramsArray);
+    }
+
+
+    private void onSignResponse(SignResponse signResponse)
+
+    {
+        Debug.Log("Retrieved SignResponse: " + signResponse);
+        updateConsole("Retrieved SignResponse: " + signResponse);
     }
 
 

234b62cfbecf9a3aa9c91992766927b4ceee9850
Maharshi Mishra <maharshi@tor.us>
2024-09-30 19:26:11 +0530

    Added wallet adapters

--- web-modal-sdk/blockchain-connection-examples/evm-modal-example/src/web3AuthProviderProps.ts
+++ web-modal-sdk/blockchain-connection-examples/evm-modal-example/src/web3AuthProviderProps.ts
@@ -2,9 +2,9 @@ import { Web3AuthContextConfig } from "@web3auth/modal-react-hooks";
 import { Web3AuthOptions } from "@web3auth/modal";
 import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
 import { CHAIN_NAMESPACES, WEB3AUTH_NETWORK } from "@web3auth/base";
-import { AuthAdapter } from "@web3auth/auth-adapter";
+import { AuthAdapter, MfaSettings } from "@web3auth/auth-adapter";
 import { WalletServicesPlugin } from "@web3auth/wallet-services-plugin";
-import { getDefaultExternalAdapters, getInjectedAdapters } from "@web3auth/default-evm-adapter";
+import { getDefaultExternalAdapters } from "@web3auth/default-evm-adapter";
 
 const chainConfig = {
   chainId: "0xaa36a7", // for wallet connect make sure to pass in this chain in the loginSettings of the adapter.
@@ -78,17 +78,26 @@ const authAdapter = new AuthAdapter({
         priority: 4,
         mandatory: true,
       },
+      passkeysFactor: {
+        enable: true,
+        priority: 5,
+        mandatory: false,
+      },
+      authenticatorFactor: {
+        enable: true,
+        priority: 6,
+        mandatory: false,
+      },
     },
   },
 });
 
 const walletServicesPlugin = new WalletServicesPlugin();
 
-// const adapters = await getInjectedAdapters({options: web3AuthOptions});
-// const adapters = await getDefaultExternalAdapters({ options: web3AuthOptions });
+const adapters = await getDefaultExternalAdapters({ options: web3AuthOptions });
 
 export const web3AuthContextConfig: Web3AuthContextConfig = {
   web3AuthOptions,
-  adapters: [authAdapter],
+  adapters: [authAdapter, ...adapters],
   plugins: [walletServicesPlugin],
 };

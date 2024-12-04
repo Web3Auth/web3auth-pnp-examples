b23938a24ee658ebd4a93939f2aeca82151f0751
Yashovardhan Agrawal <21066442+yashovardhan@users.noreply.github.com>
2024-12-04 16:43:19 +0530

    Update source code links

--- web-modal-sdk/quick-starts/react-hooks-modal-quick-start/src/App.tsx
+++ web-modal-sdk/quick-starts/react-hooks-modal-quick-start/src/App.tsx
@@ -132,13 +132,13 @@ function App() {
 
           <footer className="footer">
             <a
-              href="https://github.com/Web3Auth/web3auth-pnp-examples/tree/main/web-modal-sdk/quick-starts/react-vite-modal-quick-start"
+              href="https://github.com/Web3Auth/web3auth-pnp-examples/tree/main/web-modal-sdk/quick-starts/react-hooks-modal-quick-start"
               target="_blank"
               rel="noopener noreferrer"
             >
               Source code
             </a>
-            <a href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2FWeb3Auth%2Fweb3auth-pnp-examples%2Ftree%2Fmain%2Fweb-modal-sdk%2Fquick-starts%2Freact-vite-evm-modal-quick-start&project-name=w3a-react-vite-no-modal&repository-name=w3a-react-vite-no-modal">
+            <a href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2FWeb3Auth%2Fweb3auth-pnp-examples%2Ftree%2Fmain%2Fweb-modal-sdk%2Fquick-starts%2Freact-hooks-modal-quick-start&project-name=w3a-react-vite-no-modal&repository-name=w3a-react-vite-no-modal">
               <img src="https://vercel.com/button" alt="Deploy with Vercel" />
             </a>
           </footer>

71a422afb3aa7fe88663cddc305399da72e3e44b
Yashovardhan Agrawal <21066442+yashovardhan@users.noreply.github.com>
2024-12-04 13:13:14 +0530

    Add vue composables no modal QS

---
+++ web-no-modal-sdk/quick-starts/vue-composables-no-modal-quick-start/src/App.vue
@@ -0,0 +1,34 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import Home from "./Home.vue";
+import { Web3AuthProvider } from "@web3auth/no-modal-vue-composables";
+import useWeb3AuthConfig from "./web3authContext";
+
+const { configs } = useWeb3AuthConfig();
+
+export default defineComponent({
+  name: "App",
+  components: {
+    Home,
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+<template>
+  <div class="min-h-screen flex flex-col">
+    <Web3AuthProvider :config="configs">
+      <Home msg="Welcome to Your Vue.js + TypeScript App" />
+    </Web3AuthProvider>
+  </div>
+</template>

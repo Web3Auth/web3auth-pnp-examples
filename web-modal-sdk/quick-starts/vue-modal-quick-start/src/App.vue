335dca7741ed0a9f1582e90bead5dd0119a00138
Yashovardhan Agrawal <21066442+yashovardhan@users.noreply.github.com>
2024-12-04 16:43:09 +0530

    Update vue examples to script setup

--- web-modal-sdk/quick-starts/vue-modal-quick-start/src/App.vue
+++ web-modal-sdk/quick-starts/vue-modal-quick-start/src/App.vue
@@ -2,16 +2,8 @@
   <Home msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script setup lang="ts">
 import Home from "./Home.vue";
-
-export default defineComponent({
-  name: "App",
-  components: {
-    Home,
-  },
-});
 </script>
 
 <style>

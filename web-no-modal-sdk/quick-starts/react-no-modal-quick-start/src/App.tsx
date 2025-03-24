b8779d5261732f69a1118339a206e2382d1be1fc
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2025-03-24 15:33:34 +0530

    Update react no modal quick start to new interface

--- web-no-modal-sdk/quick-starts/react-no-modal-quick-start/src/App.tsx
+++ web-no-modal-sdk/quick-starts/react-no-modal-quick-start/src/App.tsx
@@ -2,10 +2,7 @@
 import "./App.css";
 
 // IMP START - Quick Start
-import { IAdapter, IProvider, WALLET_ADAPTERS, WEB3AUTH_NETWORK, IWeb3AuthCoreOptions, getEvmChainConfig } from "@web3auth/base";
-import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
-import { Web3AuthNoModal } from "@web3auth/no-modal";
-import { AuthAdapter } from "@web3auth/auth-adapter";
+import { Web3AuthNoModal, IProvider, WEB3AUTH_NETWORK, getEvmChainConfig, CustomChainConfig, WALLET_CONNECTORS, authConnector } from "@web3auth/no-modal";
 // IMP END - Quick Start
 import { useEffect, useState } from "react";
 
@@ -21,21 +18,19 @@ const clientId = "BPi5PB_UiIZ-cPz1GtV5i1I2iOSOHuimiXBI0e-Oe_u6X3oVAbCiAZOTEBtTXw
 
 // IMP START - Chain Config
 // Get custom chain configs for your chain from https://web3auth.io/docs/connect-blockchain
-const chainConfig = getEvmChainConfig(0xaa36a7, clientId)!;
+const chainId = 0xaa36a7; // Sepolia testnet
+const chains: CustomChainConfig[] = [getEvmChainConfig(chainId, clientId)!];
 // IMP END - Chain Config
 
 // IMP START - SDK Initialization
-const privateKeyProvider = new EthereumPrivateKeyProvider({ config: { chainConfig } });
-
-const web3AuthOptions: IWeb3AuthCoreOptions = {
+const web3auth = new Web3AuthNoModal({
   clientId,
   web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_MAINNET,
-  privateKeyProvider,
-}
-const web3auth = new Web3AuthNoModal(web3AuthOptions);
-
-const authadapter = new AuthAdapter();
-web3auth.configureAdapter(authadapter);
+  chains,
+  defaultChainId: chainId.toString(),
+  multiInjectedProviderDiscovery: true,
+  connectors: [authConnector({})]
+});
 // IMP END - SDK Initialization
 
 function App() {
@@ -64,7 +59,7 @@ function App() {
   const login = async () => {
     var web3authProvider = null;
     // IMP START - Login
-    web3authProvider = await web3auth.connectTo(WALLET_ADAPTERS.AUTH, {
+    web3authProvider = await web3auth.connectTo(WALLET_CONNECTORS.AUTH, {
       loginProvider: "google",
     });
     // IMP END - Login

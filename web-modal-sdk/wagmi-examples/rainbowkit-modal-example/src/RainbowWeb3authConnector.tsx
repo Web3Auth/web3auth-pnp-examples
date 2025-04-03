32b0cd0db5d44a90de53c55d80a57d55d447d724
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2025-04-03 12:20:18 +0530

    Add wallet services plugin in rainbowkit example

--- web-modal-sdk/wagmi-examples/rainbowkit-modal-example/src/RainbowWeb3authConnector.tsx
+++ web-modal-sdk/wagmi-examples/rainbowkit-modal-example/src/RainbowWeb3authConnector.tsx
@@ -4,6 +4,7 @@ import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
 import { CHAIN_NAMESPACES, UX_MODE, WEB3AUTH_NETWORK } from "@web3auth/base";
 import { Wallet, WalletDetailsParams } from "@rainbow-me/rainbowkit";
 import { createConnector as createWagmiConnector } from "wagmi";
+import { WalletServicesPlugin } from "@web3auth/wallet-services-plugin";
 
 const clientId =
   "BPi5PB_UiIZ-cPz1GtV5i1I2iOSOHuimiXBI0e-Oe_u6X3oVAbCiAZOTEBtTXw4tsluTITPqA8zMsfxIKMjiqNQ"; // get from https://dashboard.web3auth.io
@@ -38,6 +39,16 @@ const web3AuthInstance = new Web3Auth({
     modalZIndex: "2147483647"
   }
 });
+
+const walletServicesPlugin = new WalletServicesPlugin({
+  walletInitOptions: {
+    whiteLabel: {
+      showWidgetButton: true,
+    }
+  }
+});
+web3AuthInstance.addPlugin(walletServicesPlugin);
+
 
 export const rainbowWeb3AuthConnector = (): Wallet => ({
   id: "web3auth",

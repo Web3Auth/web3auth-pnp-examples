8796c1a32d4110f59fbb2179e20c7224ec76faac
Yashovardhan Agrawal <21066442+yashovardhan@users.noreply.github.com>
2025-05-18 17:49:34 +0530

    Remove deploy with vercel button

--- quick-starts/react-solana-quick-start/src/App.tsx
+++ quick-starts/react-solana-quick-start/src/App.tsx
@@ -79,9 +79,6 @@ function App() {
         >
           Source code
         </a>
-        <a href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2FWeb3Auth%2Fweb3auth-pnp-examples%2Ftree%2Fmain%2Fweb-modal-sdk%2Fquick-starts%2Freact-hooks-modal-quick-start&project-name=react-hooks-modal-quick-start&repository-name=react-hooks-modal-quick-start">
-          <img src="https://vercel.com/button" alt="Deploy with Vercel" />
-        </a>
       </footer>
     </div>
   );

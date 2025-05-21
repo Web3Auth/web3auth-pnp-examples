a1fe6a8801e8fcdc6ae795b266ec1892cb8a4d8c
Yashovardhan Agrawal <21066442+yashovardhan@users.noreply.github.com>
2025-05-21 14:14:16 +0530

    Add modal grouped connection example

--- custom-authentication/grouped-connection/modal-google-email-passwordless-grouped-example/src/web3authContext.tsx
+++ custom-authentication/grouped-connection/modal-google-email-passwordless-grouped-example/src/web3authContext.tsx
@@ -1,5 +1,5 @@
 // IMP START - Quick Start
-import { WALLET_CONNECTORS, WEB3AUTH_NETWORK } from "@web3auth/modal";
+import { AUTH_CONNECTION, WALLET_CONNECTORS, WEB3AUTH_NETWORK } from "@web3auth/modal";
 import { type Web3AuthContextConfig } from "@web3auth/modal/react";
 // IMP END - Quick Start
 
@@ -20,36 +20,20 @@ const web3AuthContextConfig: Web3AuthContextConfig = {
           loginMethods: {
             google: {
               name: "google login",
-              authConnectionId: "w3a-google-demo",
+              authConnectionId: "w3a-google",
+              groupedAuthConnectionId: "aggregate-sapphire",
             },
             facebook: {
               name: "facebook login",
-              authConnectionId: "w3a-facebook-demo",
-            },
-            discord: {
-              name: "discord login",
-              authConnectionId: "w3a-discord-demo",  
-            },
-            twitch: {
-              name: "twitch login",
-              authConnectionId: "w3a-twitch-demo",
-            },
-            twitter: {
-              name: "twitter login",
-              // it will hide the twitter option from the Web3Auth modal.
-              showOnModal: false,
+              authConnectionId: "w3a-facebook",
+              groupedAuthConnectionId: "aggregate-sapphire"
             },
             email_passwordless: {
               name: "email passwordless login",
-              authConnectionId: "w3a-email_passwordless-demo"
+              authConnectionId: "w3a-email-passwordless",
+              groupedAuthConnectionId: "aggregate-sapphire"
             },
-            sms_passwordless: {
-              name: "sms passwordless login",
-              authConnectionId: "w3a-sms_passwordless-demo"
-            }
           },
-          // setting it to false will hide all social login methods from modal.
-          showOnModal: true,
         }
       },
     },

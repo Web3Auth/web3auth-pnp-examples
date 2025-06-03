a3174e7a4192791b7457ffbbd4e2d36ad98be0f2
Yashovardhan Agrawal <21066442+yashovardhan@users.noreply.github.com>
2025-06-03 19:11:03 +0530

    Update SDK

--- quick-starts/react-quick-start/src/web3authContext.tsx
+++ quick-starts/react-quick-start/src/web3authContext.tsx
@@ -1,17 +1,31 @@
 // IMP START - Quick Start
-import { WEB3AUTH_NETWORK } from "@web3auth/modal";
+import { AUTH_CONNECTION, MFA_LEVELS, WALLET_CONNECTORS, WEB3AUTH_NETWORK } from "@web3auth/modal";
 import { type Web3AuthContextConfig } from "@web3auth/modal/react";
 // IMP END - Quick Start
 
 // IMP START - Dashboard Registration
-const clientId = "BPi5PB_UiIZ-cPz1GtV5i1I2iOSOHuimiXBI0e-Oe_u6X3oVAbCiAZOTEBtTXw4tsluTITPqA8zMsfxIKMjiqNQ"; // get from https://dashboard.web3auth.io
+const clientId = "BLQifFrG2BK7I719MiqtKAgTC38zPZbxPnsDrcYVJ0mrJQG9K-ev6r8Z4c83n46fRLpCwEgsqbZS5lFWyJW-eCY"; // get from https://dashboard.web3auth.io
 // IMP END - Dashboard Registration
 
 // IMP START - Config
 const web3AuthContextConfig: Web3AuthContextConfig = {
   web3AuthOptions: {
     clientId,
-    web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_MAINNET,
+    web3AuthNetwork: WEB3AUTH_NETWORK.MAINNET,
+    mfaLevel: MFA_LEVELS.NONE,
+    modalConfig: {
+      connectors: {
+        [WALLET_CONNECTORS.AUTH]: {
+          label: "auth",
+          loginMethods: {
+            google: {
+              name: "google login",
+              authConnectionId: "tlnt-google",
+            },
+          },
+        },
+      },
+    },
   }
 };
 // IMP END - Config
